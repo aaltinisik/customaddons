7437938e1e85318ce22c2ba642a9b24a9208541d
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2014-08-17 21:29:17 +0300

    added new module and cleaned pyc files

---
+++ altinkaya61/__openerp__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'ALTINKAYA',
+    'version': '1.0',
+    'author': 'Altinkaya Enclosures',
+    'website': 'http://www.altinkaya.eu',
+    'category': 'tools',
+    'sequence': 1,
+    'summary': 'Altinkaya Enclosures Customizations for V6',
+    'images': [],
+    'depends': ['account'],
+    'description': """
+Altinkaya Elektronik Cihaz Kutulari OpenERP V6.1 ozellestirmeleri
+==================================================================
+
+    laylaylom
+    
+    
+    """,
+    'data': [],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
