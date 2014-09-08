2d25824893e454af95ed479aacd4ec7a865e8363
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2014-09-08 16:48:22 +0300

    add altinkaya_payment_url functional field

--- altinkaya/model/account_invoice.py
+++ altinkaya/model/account_invoice.py
@@ -3,6 +3,31 @@ from openerp.osv import osv, fields
 
 class account_invoice(osv.Model):
     _inherit = 'account.invoice'
+
+    def _altinkaya_payment_url(self, cr, uid, ids, field, arg, context=None):
+        res = dict.fromkeys(ids, False)
+        for invoice in self.browse(cr, uid, ids, context=context):
+
+            tutar = '%d' % (int)(100*order.amount_total)
+            eposta = invoice.partner_id.email
+            if eposta is False:
+                eposta = ""
+            params = {
+                    "email": unicode(eposta),
+                    "musteri": unicode(invoice.partner_id.commercial_partner_id.name),
+                    "oid": unicode(invoice.number),
+                    "tutar": unicode(tutar),
+                    "ref": unicode(invoice.partner_id.commercial_partner_id.ref),
+                    "currency": unicode(invoice.currency_id.name),
+                    "lang": unicode(invoice.partner_id.lang),
+                    "hashtr": hashlib.sha1(unicode(invoice.currency_id.name) + unicode(invoice.partner_id.commercial_partner_id.ref) + unicode(eposta) + unicode(tutar) + unicode(invoice.number) + unicode(invoice.company_id.hash_code)).hexdigest().upper(),
+                    }
+            res[order.id] = "?" + url_encode(params)
+        return res
+
+
+
+
     _columns = {
         'x_serino': fields.char(
             'Fatura No',
@@ -16,8 +41,8 @@ class account_invoice(osv.Model):
         'address_contact_id': fields.many2one(
             'res.partner',
             'Shipping Address'
-
             ),
+        'altinkaya_payment_url': fields.function(_altinkaya_payment_url, type='char', string='Altinkaya Payment Url'),
                 }
 
 account_invoice()
