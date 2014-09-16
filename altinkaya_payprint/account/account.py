4bd5652f5e0fe23396bf6de2762f465eb8f02d52
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2014-09-16 11:51:42 +0300

    New module to add pay & print button on invoice register payment wizard

---
+++ altinkaya_payprint/account/account.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2013-Present Acespritech Solutions Pvt. Ltd. (<http://acespritech.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+##############################################################################
+
+from openerp.osv import fields, osv, orm
+from openerp import netsvc
+
+
+class account_voucher(osv.osv):
+    _inherit = "account.voucher"
+
+    def button_proforma_voucher_report(self, cr, uid, ids, context=None):
+        context = context or {}
+        action_pool = self.pool.get('ir.actions.server')
+        wf_service = netsvc.LocalService("workflow")
+        for vid in ids:
+            wf_service.trg_validate(uid, 'account.voucher', vid, 'proforma_voucher', cr)
+        # custom code from here
+
+#        report_action_id = 558
+#        context['report_action_id'] = report_action_id
+#        print_actions_obj = self.pool.get('aeroo.print_actions')
+#        printer = print_actions_obj._get_default_printer(cr, uid, context)
+#        print_actions_obj.report_to_printer(cr, uid, ids, report_action_id, printer, context=context)
+
+        server_action_ids = [843]
+        server_action_ids = map(int, server_action_ids)
+        action_server_obj = self.pool.get('ir.actions.server')
+        ctx = dict(context, active_model='account.voucher', active_ids=ids, active_id=ids[0])
+        action_server_obj.run(cr, uid, server_action_ids, context=ctx)
+
+        return {'type': 'ir.actions.act_window_close'}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
