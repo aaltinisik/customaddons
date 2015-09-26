80875b4f9f13245f8c4b294d20b731c11f545590
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-09-26 19:02:17 +0300

    removed unused modules moved all folders to unported subfolder

--- altinkaya61/model/res_partner.py
+++
@@ -1,8 +0,0 @@
-from openerp.osv import osv, fields
-
-
-class res_partner(osv.Model):
-    _inherit = 'res.partner'
-    _columns = {
-         'z_muhasebe_kodu': fields.char('Zirve Muhasebe kodu', size=64, required=False, translate=False)
-    }
--- altinkaya_hscode/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import hscode
-import product_category
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
