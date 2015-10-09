949b475b9cbb0b7d2d2753a3523517ab318b9c34
st6 <st6@st6-PC.(none)>
2015-10-09 18:31:41 +0530

    [IMP]Migrate altinkaya_followup in V8

---
+++ altinkaya_followup/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'altinkaya_followup',
+    'version': '1.0',
+    'author': 'Altinkaya Enclosures',
+    'website': 'http://www.altinkaya.eu',
+    'category': 'altinkaya',
+    'sequence': 1,
+    'summary': 'Altinkaya Enclosures Account followup additions V8',
+    'images': [],
+    'depends': ['account_followup','base_phone'],
+    'description': """
+Altinkaya Elektronik Cihaz Kutulari Account Followup eklentileri
+===============================================================
+
+
+
+    """,
+    'data': [
+    "view/altinkaya_view_partner.xml",
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
