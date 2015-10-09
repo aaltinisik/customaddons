39f7e4ba46d6375b7d19209b32adcd8445602837
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-10-09 11:14:04 +0300

    New module for adding oldprice fields to product template model

---
+++ alinkaya_oldprice/__openerp__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'ALTINKAYA Old Prices',
+    'version': '1.0',
+    'author': 'Altinkaya Enclosures',
+    'website': 'http://www.altinkaya.eu',
+    'category': 'altinkaya',
+    'sequence': 1,
+    'summary': 'Altinkaya add fields for old prices for V7',
+    'images': [],
+    'depends': ['product'],
+    'description': """
+Altinkaya Elektronik Cihaz Kutulari Eski fiyat alanlari Ekler
+===============================================================
+
+
+
+    """,
+    'data': [
+    "view/altinkaya_view_product.xml",
+    ],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
