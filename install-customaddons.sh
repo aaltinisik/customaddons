9b0f4f5b9cfd732c6baf12a1ba370a1beda1b931
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2015-12-13 12:16:37 +0200

    new script to update repositories and update DB

--- install-customaddons.sh
+++ install-customaddons.sh
@@ -37,6 +37,7 @@ while true; do
         git clone -b 8.0 https://github.com/OCA/sale-workflow.git $OCA_HOME/sale-workflow
         git clone -b 8.0 https://github.com/OCA/account-invoicing.git $OCA_HOME/account-invoicing
         git clone -b 8.0 https://github.com/OCA/stock-logistics-tracking.git $OCA_HOME/stock-logistics-tracking
+        git clone -b 8.0 https://github.com/OCA/partner-contact.git $OCA_HOME/partner-contact
        
         break;;
         [Nn]* ) break;;
@@ -51,19 +52,16 @@ while true; do
         ln -s -f $OCA_HOME/connector-telephony/* $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/aeroo_reports/* $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/aeroo_reports/* $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/web/web_widget_many2many_tags_multi_selection $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/web/web_translate_dialog $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/web/web_sheet_full_width $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/web/web_searchbar_full_width $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/web/web_last_viewed_records $OE_HOME/custom/addons/
-       
         ln -s -f $OCA_HOME/server-tools/auth_admin_passkey $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/base_concurrency $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/cron_run_manually $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/web/web_environment_ribbon $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/scheduler_error_mailer $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/server-tools/base_optional_quick_create $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/base_report_auto_create_qweb $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/disable_openerp_online $OE_HOME/custom/addons/
@@ -72,18 +70,13 @@ while true; do
         ln -s -f $OCA_HOME/server-tools/module_prototyper $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/mass_editing $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/server-tools/super_calendar $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/knowledge/attachment_preview $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/knowledge/attachments_to_filesystem $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/purchase-workflow/product_by_supplier $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/purchase-workflow/purchase_order_revision $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/partner-contact/partner_external_maps $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/product-attribute/product_dimension $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/product-attribute/product_weight $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/sale-workflow/partner_prepayment $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/sale-workflow/sale_automatic_workflow $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/sale-workflow/sale_cancel_reason $OE_HOME/custom/addons/
@@ -97,7 +90,6 @@ while true; do
         ln -s -f $OCA_HOME/sale-workflow/sale_partner_incoterm $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/sale-workflow/sale_payment_method $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/sale-workflow/sale_validity $OE_HOME/custom/addons/
-
         ln -s -f $OCA_HOME/account-invoicing/account_invoice_partner $OE_HOME/custom/addons/
         ln -s -f $OCA_HOME/stock-logistics-tracking/stock_barcode_reader $OE_HOME/custom/addons/
     
