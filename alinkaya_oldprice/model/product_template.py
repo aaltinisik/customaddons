d6064d88460619a7198a9ba1bae7d16fa6343ffe
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2016-12-30 18:45:13 +0300

    added price

--- alinkaya_oldprice/model/product_template.py
+++ alinkaya_oldprice/model/product_template.py
@@ -19,5 +19,20 @@ class product_template(osv.Model):
              u"2015 Ocak Min İşçcilik",
              digits_compute=dp.get_precision('Product Price'),
              help=u"2015 Ocak kullanılan eski Minimum İşçilik fiyatı"),
+        'z_fiyat_2016a': fields.float(
+             u"2016 Aralik Eski Fiyati",
+             digits_compute=dp.get_precision('Product Price'),
+             help=u"2016 Aralik eski fiyati"),
+        'z_2016a_iscilik': fields.float(
+             u"2016 Aralik iscilik Fiyati",
+             digits_compute=dp.get_precision('Product Price'),
+             help=u"2016 Aralik kullanılan birim iscilik fiyatı"),
+        'z_min_2016a_iscilik': fields.float(
+             u"2016 Aralik Min İscilik",
+             digits_compute=dp.get_precision('Product Price'),
+             help=u"2016 Aralık kullanılan eski Minimum İscilik fiyatı"),
+
+
+
     }
 product_template()
