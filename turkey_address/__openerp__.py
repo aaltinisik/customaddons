8b327f2f0db463c53949c5fff6b53951557307b6
Your Name <Kiran Kantesariya>
2017-06-30 12:09:55 +0530

    add module for turkey address

---
+++ turkey_address/__openerp__.py
@@ -0,0 +1,17 @@
+#Custome Module for Ahmet 
+{
+    'name': 'Turkey Address',
+    'version': '8.1',
+    'summary': "Add custome field in Customer like District, Resion, Neighbourhood",
+    'description': """This Module is used to add custom field in res.partner Object""",
+    'category': 'Sale',
+    'author': 'Kiran Kantesariya',
+    'Email': 'kiran.backup0412@gmail.com',
+    'Skype': 'kiran.backup0412@gmail.com',
+    'depends': ['sale'],
+    'data': [
+        'res_partner_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
