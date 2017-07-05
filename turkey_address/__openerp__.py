b889f812b0c41ff5fd11f79a63ba4cb0475af8d5
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-05 08:54:57 +0300

    Updated turkey_address module translation added new field for country address generation added display address function with newly added fields TODO added xml data for country address data failed to run

--- turkey_address/__openerp__.py
+++ turkey_address/__openerp__.py
@@ -1,8 +1,7 @@
-#Custome Module for Ahmet 
 {
     'name': 'Turkey Address',
     'version': '8.0.2',
-    'summary': "Add custome field in Customer like District, Resion, Neighbourhood",
+    'summary': "Add custom field in Customer like District, Resion, Neighbourhood",
     'description': """This Module is used to add custom field in res.partner Object""",
     'category': 'Sale',
     'author': 'Kirankumar',
@@ -10,6 +9,7 @@
     'data': [
         'address_details_view.xml',
         'res_partner_view.xml',
+#        'res_country_data.xml',
     ],
     'installable': True,
     'auto_install': False
