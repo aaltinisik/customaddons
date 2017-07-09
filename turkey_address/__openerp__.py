87d0d92dbea119742f64d40b71f3095e0dce38d4
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-09 11:36:47 +0300

    added dependency

--- turkey_address/__openerp__.py
+++ turkey_address/__openerp__.py
@@ -4,7 +4,8 @@
     'summary': "Add custom field in Customer like District, Resion, Neighbourhood",
     'description': """This Module is used to add custom field in res.partner Object""",
     'category': 'Sale',
-    'author': 'Kirankumar',
+    'author': 'Kirankumar,Ahmet Altinisik',
+    'external_dependencies': {'python': ['xlrd']},
     'depends': ['sale'],
     'data': [
         'address_details_view.xml',
