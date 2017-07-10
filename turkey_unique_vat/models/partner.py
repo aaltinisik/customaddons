8f04819a143086364f490c79f92963ee24229c61
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-10 21:15:06 +0300

    altered vat check function with super

--- turkey_unique_vat/models/partner.py
+++ turkey_unique_vat/models/partner.py
@@ -1,96 +1,29 @@
-import logging
-import string
-import datetime
-import re
-_logger = logging.getLogger(__name__)
-
-try:
-    import vatnumber
-except ImportError:
-    _logger.warning("VAT validation partially unavailable because the `vatnumber` Python library cannot be found. "
-                                          "Install it to support more countries, for example with `easy_install vatnumber`.")
-    vatnumber = None
-
 from openerp.osv import fields, osv
-from openerp.tools.misc import ustr
 from openerp.tools.translate import _
-
-_ref_vat = {
-    'at': 'ATU12345675',
-    'be': 'BE0477472701',
-    'bg': 'BG1234567892',
-    'ch': 'CHE-123.456.788 TVA or CH TVA 123456', #Swiss by Yannick Vaucher @ Camptocamp
-    'cy': 'CY12345678F',
-    'cz': 'CZ12345679',
-    'de': 'DE123456788',
-    'dk': 'DK12345674',
-    'ee': 'EE123456780',
-    'el': 'EL12345670',
-    'es': 'ESA12345674',
-    'fi': 'FI12345671',
-    'fr': 'FR32123456789',
-    'gb': 'GB123456782',
-    'gr': 'GR12345670',
-    'hu': 'HU12345676',
-    'hr': 'HR01234567896', # Croatia, contributed by Milan Tribuson 
-    'ie': 'IE1234567FA',
-    'it': 'IT12345670017',
-    'lt': 'LT123456715',
-    'lu': 'LU12345613',
-    'lv': 'LV41234567891',
-    'mt': 'MT12345634',
-    'mx': 'MXABC123456T1B',
-    'nl': 'NL123456782B90',
-    'no': 'NO123456785',
-    'pe': 'PER10254824220 or PED10254824220',
-    'pl': 'PL1234567883',
-    'pt': 'PT123456789',
-    'ro': 'RO1234567897',
-    'se': 'SE123456789701',
-    'si': 'SI12345679',
-    'sk': 'SK0012345675',
-    'tr': 'TR1234567890 (VERGINO) veya TR12345678901 (TCKIMLIKNO)' # Levent Karakas @ Eska Yazilim A.S.
-}
-
 
 class res_partner(osv.osv):
     _inherit = 'res.partner'
 
     def check_vat(self, cr, uid, ids, context=None):
-        user_company = self.pool.get('res.users').browse(cr, uid, uid).company_id
-        if user_company.vat_check_vies:
-            # force full VIES online check
-            check_func = self.vies_vat_check
-        else:
-            # quick and partial off-line checksum validation
-            check_func = self.simple_vat_check
+        for partner in self.browse(cr, uid, ids, context=context):
+            if partner.sanitized_vat == '11111111111':
+                continue
+            if not partner.vat:
+                continue
+            colliding_partner = self.search(cr, uid, [('sanitized_vat', '=', partner.sanitized_vat),('id', '!=', partner.id)],limit=1, context=context)
+            colliding_partner = self.browse(cr, uid, colliding_partner)
+            if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
+                return False
+        return super(res_partner, self).check_vat(cr, uid, ids, context=context)
+
+    def _construct_constraint_msg(self, cr, uid, ids, context=None):
         for partner in self.browse(cr, uid, ids, context=context):
             if not partner.vat:
                 continue
-            vat_country, vat_number = self._split_vat(partner.vat)
-            if vat_number and partner.company_id:
-                colliding_partner = self.search(cr, uid, [('vat', '=', partner.vat), ('id', '!=', partner.id)], limit=1, context=context)
-                colliding_partner = self.browse(cr, uid, colliding_partner)
-                if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
-                    raise osv.except_osv(_('Warning!'),_("This VAT Number is already assingn to [%s] partner.") % (colliding_partner.name))
-            if not check_func(cr, uid, vat_country, vat_number, context=context):
-                _logger.info(_("Importing VAT Number [%s] is not valid !" % vat_number))
-                return False
-        return True
-
-    def _construct_constraint_msg(self, cr, uid, ids, context=None):
-        def default_vat_check(cn, vn):
-            # by default, a VAT number is valid if:
-            #  it starts with 2 letters
-            #  has more than 3 characters
-            return cn[0] in string.ascii_lowercase and cn[1] in string.ascii_lowercase
-        vat_country, vat_number = self._split_vat(self.browse(cr, uid, ids)[0].vat)
-        vat_no = "'CC##' (CC=Country Code, ##=VAT Number)"
-        error_partner = self.browse(cr, uid, ids, context=context)
-        if default_vat_check(vat_country, vat_number):
-            vat_no = _ref_vat[vat_country] if vat_country in _ref_vat else vat_no
-            if self.pool['res.users'].browse(cr, uid, uid).company_id.vat_check_vies:
-                return '\n' + _('The VAT number [%s] for partner [%s] either failed the VIES VAT validation check or did not respect the expected format %s.') % (error_partner[0].vat, error_partner[0].name, vat_no)
-        return '\n' + _('The VAT number [%s] for partner [%s] does not seem to be valid. \nNote: the expected format is %s') % (error_partner[0].vat, error_partner[0].name, vat_no)
+            colliding_partner = self.search(cr, uid, [('sanitized_vat', '=', partner.sanitized_vat),('id', '!=', partner.id)],limit=1, context=context)
+            colliding_partner = self.browse(cr, uid, colliding_partner)
+            if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
+                return '\n' + _('The VAT number [%s] for this partner is also registered with partner [%s] Each partner should have unique vat number.') % (colliding_partner[0].vat,colliding_partner[0].name)
+            return '\n' + _('The VAT number [%s] for partner [%s] does not seem to be valid. ') % (partner.vat, partner.name)
 
     _constraints = [(check_vat, _construct_constraint_msg, ["vat"])]
