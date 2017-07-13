a616fd1040da5bc651914b6b8bdb150f6147c34b
Your Name <Kiran Kantesariya>
2017-07-13 14:35:07 +0530

    [ADD] add upated code

--- altinkaya_worksector/models/partner.py
+++ altinkaya_worksector/models/partner.py
@@ -1,3 +1,4 @@
+# from openerp import api, fields, models, _
 from openerp.osv import osv, fields
 
 class res_partner_worksector(osv.osv):
@@ -8,6 +9,7 @@ class res_partner_worksector(osv.osv):
         'name' : fields.char(string="Name", translate=True),
         'partner_ids' : fields.one2many('worksector.line', 'worksector_id', string="Partner"),
         'product_categ_ids' : fields.one2many('product.category.line', 'worksector_id', string="Category"),
+#        'product_categ_ids': fields.many2many('product.category.line', 'partner_sector_prod_categ_line_rel', 'partner_wrksector_id', 'categ_line_id', string="Category")
     }
 
 
@@ -24,7 +26,8 @@ class res_partner(osv.osv):
         return res
 
     _columns = {
-        'worksector_ids' : fields.one2many('worksector.line', 'partner_id', string="Worksector"),
+#        'worksector_ids' : fields.one2many('worksector.line', 'partner_id', string="Worksector"),
+        'worksector_ids' : fields.many2many('worksector.line', 'partner_work_sector_line_rel', 'partner_id', 'work_sector_line_id', string="Worksector"),
         'target_product_categ_ids' : fields.function(_get_product_categ, string='Entry Lines', type='many2many', relation='product.category'),
     }
 
@@ -44,4 +47,14 @@ class product_category_line(osv.osv):
     _columns = {
         'product_categ_id' : fields.many2one('product.category', string="Product Category"),
         'worksector_id' : fields.many2one('res.partner.worksector', string="worksector"),
+    }
+
+
+class product_category(osv.osv):
+    _inherit = 'product.category'
+
+    _columns = {
+        'product_categ_id': fields.many2one('product.category', string="Product Category"),
+#        'product_category_ids': fields.one2many('product.category', 'product_categ_id', string="Categories")
+        'product_category_ids': fields.many2many('product.category', 'prod_categ_rel', 'partner_wrksector_id', 'categ_id', string="Categories")
     }
