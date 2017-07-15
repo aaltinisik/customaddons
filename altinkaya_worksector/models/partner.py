8cd96d269c7d071ecac77c3a737f228ab6efdc8c
Your Name <Kiran Kantesariya>
2017-07-15 23:26:04 +0530

    changed worksector module

--- altinkaya_worksector/models/partner.py
+++ altinkaya_worksector/models/partner.py
@@ -1,40 +1,52 @@
-from openerp import api, fields, models, _
+# from openerp import api, fields, models, _
+
+from openerp.osv import fields, osv
 
 
-class res_partner_worksector(models.Model):
-    _name = 'res.partner.worksector'
+class res_partner_worksector(osv.osv):
+    _name = 'worksector'
 
-    description = fields.Text(string="Description", translate=True)
-    name = fields.Char(string="Name", translate=True)
-    partner_ids = fields.One2many('worksector.line', 'worksector_id', string="Partner")
-    product_categ_ids = fields.One2many('product.category.line', 'worksector_id', string="Category")
+    _columns = {
+        'description': fields.text(string="Description", translate=True),
+        'name': fields.char(string="Name", translate=True),
+        'partner_ids': fields.many2many('res.partner', 'table_worksector_partner_rel', 'wid', 'partid', string="Partner"),
+        'product_categ_ids': fields.many2many('product.category', string="Category")
+    }
 
 
-class res_partner(models.Model):
+class res_partner(osv.osv):
     _inherit = 'res.partner'
 
-    @api.multi
-    @api.depends('worksector_ids')
-    def _get_product_categ(self):
-        for part in self:
+    def _get_product_categ(self, cr, uid, ids, field_name, arg, context=None):
+        res = {}
+        recids = self.browse(cr, uid, ids)
+        for part in recids:
             lst = []
             for line in part.worksector_ids:
-                lst += [x.product_categ_id.id for x in line.worksector_id.product_categ_ids]
-            part.target_product_categ_ids = self.env['product.category'].browse(set(lst))
+                lst += [x.id for x in line.product_categ_ids]
+#             part.target_product_categ_ids = self.env['product.category'].browse(cr, uid, set(lst))
+            res[part.id] = list(set(lst))
+        return res
 
-    worksector_ids = fields.One2many('worksector.line', 'partner_id', string="Worksector")
-    target_product_categ_ids = fields.Many2many('product.category', compute="_get_product_categ", store=True)
-
-
-class worksector_line(models.Model):
-    _name = 'worksector.line'
-
-    partner_id = fields.Many2one('res.partner', string="Partner")
-    worksector_id = fields.Many2one('res.partner.worksector', string="worksector")
+    _columns = {
+        'worksector_ids': fields.many2many('worksector', 'table_worksector_partner_rel', 'partid', 'wid', string="Worksector"),
+        'target_product_categ_ids': fields.function(_get_product_categ, type="many2many", relation='product.category', string="Target Product Category")
+    }
 
 
-class product_category_line(models.Model):
-    _name = 'product.category.line'
 
-    product_categ_id = fields.Many2one('product.category', string="Product Category")
-    worksector_id = fields.Many2one('res.partner.worksector', string="worksector")
+
+
+
+# class worksector_line(models.Model):
+#     _name = 'worksector.line'
+# 
+#     partner_id = fields.Many2one('res.partner', string="Partner")
+#     worksector_id = fields.Many2one('worksector', string="worksector")
+
+
+# class product_category_line(models.Model):
+#     _name = 'product.category.line'
+# 
+#     product_categ_id = fields.Many2one('product.category', string="Product Category")
+#     worksector_id = fields.Many2one('worksector', string="worksector")
