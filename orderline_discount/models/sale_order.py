9f247f4e38c05f78bbf6e8097dee1553d07ea558
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-21 22:18:39 +0300

    changed orderline

--- orderline_discount/models/sale_order.py
+++ orderline_discount/models/sale_order.py
@@ -1,39 +1,20 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from openerp import api, fields, models, _
+import openerp.addons.decimal_precision as dp
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    @api.onchange('price_unit', 'discount')
-    # @api.onchange('product_uom_qty', 'price_unit', 'discount')
-    def onchange_discount_price(self):
-        total_discount_price = 0.0
-        if self.discount:
-#            total_discount_price = (self.product_uom_qty * self.price_unit * self.discount) / 100
-            total_discount_price = (self.price_unit * (100 - self.discount)) / 100
-        self.discount_unit_price = total_discount_price
+    unit_discounted = fields.Float('Discounted Unit Price',
+                                   compute='_compute_unit_discounted',
+                                   digits_compute=dp.get_precision('Product Price'),
+                                   readonly=True, states={'draft': [('readonly', False)]},
+                                   store=False)
 
-    discount_unit_price = fields.Float('Discount Unit Price')
+    @api.one
+    @api.depends('price_unit', 'discount')
+    def _compute_unit_discounted(self):
+        self.unit_discounted = self.price_unit * ((100.0 -self.discount) / 100)
 
-    @api.model
-    def create(self, vals):
-        # qty = vals.get('product_uom_qty') or 0.0
-        price = vals.get('price_unit') or 0.0
-        discount = vals.get('discount') or 0.0
-#        total_discount_price = (qty * price * discount) / 100
-        total_discount_price = (price * (100 - discount)) / 100
-        vals.update({'discount_unit_price': total_discount_price})
-        return super(SaleOrderLine, self).create(vals)
-
-    @api.multi
-    def write(self, vals):
-        # qty = vals.get('product_uom_qty') or self.product_uom_qty
-        price = vals.get('price_unit') or self.price_unit
-        discount = vals.get('discount') or self.discount
-#        total_discount_price = (qty * price * discount) / 100
-        total_discount_price = (price * (100 - discount)) / 100
-        vals.update({'discount_unit_price': total_discount_price})
-        return super(SaleOrderLine, self).write(vals)
