05e1a1add7ce4f89763f9bd1b7d6d9717c4839cb
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-23 08:58:24 +0300

    corrected orderline_discount rounding error deleted invoice export since its duplicate

--- orderline_discount/models/sale_order.py
+++ orderline_discount/models/sale_order.py
@@ -8,13 +8,12 @@ class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     unit_discounted = fields.Float('Discounted Unit Price',
-                                   compute='_compute_unit_discounted',
                                    digits_compute=dp.get_precision('Product Price'),
+                                   compute='_compute_unit_discounted',
                                    readonly=True, states={'draft': [('readonly', False)]},
-                                   store=False)
+                                   store=True)
 
     @api.one
     @api.depends('price_unit', 'discount')
     def _compute_unit_discounted(self):
-        self.unit_discounted = self.price_unit * ((100.0 -self.discount) / 100)
-
+        self.unit_discounted = round((self.price_unit * ((100.0 -self.discount) / 100.0)),4)
