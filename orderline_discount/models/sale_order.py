3641b5aabd2ac9bed7f9933b83de20c4956af1e3
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2017-07-23 19:38:04 +0300

    modified orderline final

--- orderline_discount/models/sale_order.py
+++ orderline_discount/models/sale_order.py
@@ -17,37 +17,3 @@ class SaleOrderLine(models.Model):
     @api.depends('price_unit', 'discount')
     def _compute_unit_discounted(self):
         self.unit_discounted = round((self.price_unit * ((100.0 -self.discount) / 100.0)),4)
-
-
-# from openerp import api, fields, models, _
-
-
-# class SaleOrderLine(models.Model):
-#     _inherit = 'sale.order.line'
-
-#     discount_unit_price = fields.Float('Discount Unit Price')
-
-#     @api.onchange('price_unit', 'discount')
-#     def onchange_discount_price(self):
-#         total_discount_price = 0.0
-#         if self.discount:
-#             total_discount_price = (self.price_unit * (100 - self.discount)) / 100
-#         self.discount_unit_price = total_discount_price
-
-#     @api.model
-#     def create(self, vals):
-#         price = vals.get('price_unit') or 0.0
-#         discount = vals.get('discount') or 0.0
-#         total_discount_price = (price * (100 - discount)) / 100
-#         vals.update({'discount_unit_price': total_discount_price})
-#         return super(SaleOrderLine, self).create(vals)
-
-#     @api.multi
-#     def write(self, vals):
-#         print '>>>>>>>>>>>>>>>>>>>>>', vals
-#         if not vals.get('discount_unit_price'):
-#             price = vals.get('price_unit') or self.price_unit
-#             discount = vals.get('discount') or self.discount
-#             total_discount_price = (price * (100 - discount)) / 100
-#             vals.update({'discount_unit_price': total_discount_price})
-#         return super(SaleOrderLine, self).write(vals)
--- orderline_discount/wizard/update_discount.py
+++ orderline_discount/wizard/update_discount.py
@@ -1,4 +1,5 @@
 from openerp import models, fields, api, _
+import openerp.addons.decimal_precision as dp
 
 
 class update_discount(models.TransientModel):
@@ -11,17 +12,19 @@ class update_discount(models.TransientModel):
         active_id = context.get('active_id')
         so_line_rec = self.env['sale.order.line'].browse(active_id)
         if so_line_rec.unit_discounted:
-            res.update({'discount_price': so_line_rec.unit_discounted})
+            res.update({'unit_discounted': so_line_rec.unit_discounted,
+                        'price_unit': so_line_rec.price_unit})
         return res
 
-    discount_price = fields.Float('Discount Price')
+    unit_discounted = fields.Float('Discount Price',
+                                  digits_compute=dp.get_precision('Product Price'))
+    price_unit = fields.Float('Unit Price',
+                                  digits_compute=dp.get_precision('Product Price'))
 
     @api.multi
     def update_discount_price(self):
         context = dict(self._context)
         active_id = context.get('active_id')
-        print ':::CCC', context
         so_line_rec = self.env['sale.order.line'].browse(active_id)
-        print '::so_line_rec', so_line_rec, self.discount_price
-        so_line_rec.write({'unit_discounted': self.discount_price})
+        so_line_rec.write({'discount': ( 100.0 - (100.0 * (self.unit_discounted / self.price_unit)))})
         return True
