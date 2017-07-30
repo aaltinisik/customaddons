4c2721489cce47e301fd054644ae1cbbfe8d1ab0
Your Name <Kiran Kantesariya>
2017-07-30 08:39:46 +0530

    updated report changes

--- altinkaya_warehouse/__openerp__.py
+++ altinkaya_warehouse/__openerp__.py
@@ -10,7 +10,9 @@
     'depends': ['base','stock','mrp'],
     'data': [
              'views/product_view.xml',
-             'views/report_stockpicking.xml',
+#              'views/report_stockpicking.xml',
+             'views/report.xml',
+             'views/report_new_stock_picking_temp.xml'
              ],
     'installable': True,
     'auto_install': False
