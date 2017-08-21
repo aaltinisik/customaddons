fbab63ec4d135f710f80258edd539590f02d0532
Your Name <Kiran Kantesariya>
2017-08-21 23:49:40 +0530

    rm changes

--- altinkaya_warehouse/models/product.py
+++
@@ -1,8 +0,0 @@
-
-from openerp import models, fields, api, _
-
-
-class product_template(models.Model):
-    _inherit = "product.template"
-
-    pop_bom_in_reports = fields.Boolean(string="Populate BOM in Picking Report")
