e711cdb43e7b72fe1d46bcfa7632fba6a1e2a382
Your Name <Kiran Kantesariya>
2017-09-29 12:34:42 +0530

    Add Module to commit

---
+++ stock_picking_merge/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Stock Picking Merge',
+    'version': '8.0',
+    'website': 'https://www.odoo.com',
+    'category': 'Stock',
+    'summary': 'Stock Picking Merge',
+    'author': 'Kiran Kantesariya',
+    'description': """
+     """,
+    'depends': [
+        'stock_account', 'document'
+    ],
+    'data': [
+        'wizard/picking_merge_wiz_view.xml'
+               ],
+    'installable' : True,
+    'auto_install' : False,
+}
