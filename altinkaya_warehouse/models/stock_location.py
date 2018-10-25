b05361ea8371e8d80bbe9f75bef3e7abb325dc6c
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2018-10-25 20:28:18 +0300

    added location sort

--- altinkaya_warehouse/models/stock_location.py
+++ altinkaya_warehouse/models/stock_location.py
@@ -10,7 +10,7 @@ from openerp import exceptions
 
 class stock_location(models.Model):
     _inherit = 'stock.location'
-    _order = 'parent_left,name'
+    _order = 'loc_barcode'
     
     @api.multi
     def button_barcode_generate(self):
