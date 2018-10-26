43a99714167300a71ff9550116ba188fdd5de175
Doğan ALTUNBAY <dogan.altunbay@codequarters.com>
2018-10-26 14:03:59 +0300

    fix propagation of priority to consume lines from mrp.production

--- altinkaya_mrp/model/mrp_production.py
+++ altinkaya_mrp/model/mrp_production.py
@@ -11,3 +11,12 @@ class MrpProduction(models.Model):
         if self.routing_id.location_id:
             self.location_src_id = self.routing_id.location_id
             self.location_dest_id = self.routing_id.location_id
+            
+            
+    @api.model
+    def _make_consume_line_from_data(self, production, product, uom_id, qty,
+                                     uos_id, uos_qty):
+        move_id = super(MrpProduction, self)._make_consume_line_from_data(
+            production, product, uom_id, qty, uos_id, uos_qty)
+        self.env['stock.move'].browse([move_id]).priority = production.priority
+        return move_id
