f3ac7ba8356ca97539a7e68e546becf37f0970b4
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2018-12-05 07:57:17 +0300

    added update db script

---
+++ updateModule.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$(id -u)" = "0" ]; then
+   echo "This script must not be run as root" 1>&2
+   exit 1
+fi
+
+while true; do
+    read -p "Would you like to update modules, odoo server will stop during process  (y/n)?" yn
+    case $yn in
+        [Yy]* ) 
+	echo -e "stopping odoo server"
+        sudo systemctl stop odoo
+        echo -e "Updating Modules"
+        cd /opt/odoo/odoo-server
+        /opt/odoo/odoo-server/openerp-server -d $1 -u $2 --stop-after-init --config=/etc/odoo-server.conf --workers=0 --max-cron-threads=0
+	sudo tail  /var/log/odoo/odoo-server.log
+        echo *e "Starting odoo server"
+	sudo systemctl start odoo
+        break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
