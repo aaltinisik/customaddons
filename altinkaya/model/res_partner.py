1cee6920af94ac6dd4cc802c4842d2236558e23e
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2018-12-27 12:27:13 +0300

    added new field devir_yapildi to partner.

--- altinkaya/model/res_partner.py
+++ altinkaya/model/res_partner.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 from openerp.osv import osv, fields
 
 
@@ -6,6 +8,7 @@ class res_partner(osv.Model):
     _columns = {
     'x_vergidairesi': fields.char(u'Vergi Dairesi', size=64, required=False),
     'x_vergino': fields.char(u'Vergi No', size=64, required=False,select=1),
+    'devir_yapildi': fields.boolean(u'Devir yapıldı'),
     }
 
 
