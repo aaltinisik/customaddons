ebf4d716b5ec2b520252e44a05bbe35e1af2caaf
onurugur <onurugur@gitlab.com>
2019-01-10 17:47:16 +0300

    partner worksector translations and stock extensions aka warehouse_filters added

---
+++ stock_extensions/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Stock Extensions',
+    'version': '12.0',
+    'website': 'https://www.altinkaya.com.tr',
+    'author':'Ahmet Altınışık,OnurUgur,Codequarters',
+    'category': 'Stock',
+    'summary': 'add specialized stock extensions',
+    'description': """
+     """,
+    'depends': [
+        'stock'
+    ],
+    'data': [
+            'views/stock_picking_views.xml',
+    ],
+    'installable' : True,
+    'auto_install' : False,
+}
