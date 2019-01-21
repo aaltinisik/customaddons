d043b37245d11d1d20d63754ce731bdbf302a6ec
onurugur <onurugur@gitlab.com>
2019-01-21 16:22:17 +0300

    should check set products

--- altinkaya_sales/models/sale_order.py
+++ altinkaya_sales/models/sale_order.py
@@ -50,19 +50,20 @@ class SaleOrder(models.Model):
                     }
             order.altinkaya_payment_url = "?" + url_encode(params)
         
-    
-    @api.multi
-    def write(self, vals):
-        res = super(SaleOrder, self).write(vals)
-        self.order_line.explode_set_contents()
-        return res
-    
-    @api.model
-    def create(self, vals):
-        res = super(SaleOrder, self).create(vals)
-        res.order_line.explode_set_contents()
-        return res
-    
+ 
+#TDE Fix Onur set Products  
+#     @api.multi
+#     def write(self, vals):
+#         res = super(SaleOrder, self).write(vals)
+#         self.order_line.explode_set_contents()
+#         return res
+#     
+#     @api.model
+#     def create(self, vals):
+#         res = super(SaleOrder, self).create(vals)
+#  #       res.order_line.explode_set_contents()
+#         return res
+#     
     
 
 
@@ -76,11 +77,11 @@ class sale_order_line(models.Model):
     @api.depends('product_id')
     def _compute_set_product(self):
         bom_obj = self.env['mrp.bom'].sudo()
-        bom_id = bom_obj._bom_find(product_id=self.product_id.id, properties=self.property_ids)
+        bom_id = bom_obj._bom_find(product=self.product_id)
         if not bom_id:
             self.set_product = False
         else:
-            bom_id = bom_obj.browse(bom_id)
+            #bom_id = bom_obj.browse(bom_id.id)
             self.set_product = bom_id.type == 'phantom'
         
     
@@ -97,67 +98,67 @@ class sale_order_line(models.Model):
         return {'domain':{'product_tmpl_id':domain}}
     
     
+    #TDE Fix Onur check ask 
     
-    @api.multi
-    def explode_set_contents(self):
-        """ Explodes order lines.
-        """
-        
-        bom_obj = self.env['mrp.bom'].sudo()
-        prod_obj = self.env["product.product"].sudo()
-        uom_obj = self.env["product.uom"].sudo()
-        to_unlink_ids = self.env['sale.order.line']
-        to_explode_again_ids = self.env['sale.order.line']
-            
-        for line in self.filtered(lambda l: l.set_product == True):
-            property_ids = line.property_ids
-            bom_id = bom_obj._bom_find(product_id=line.product_id.id, properties=property_ids)
-            if not bom_id:
-                continue
-            bom_id = bom_obj.browse(bom_id)
-            if bom_id.type == 'phantom':
-                factor = uom_obj._compute_qty(line.product_uom.id, line.product_uom_qty, bom_id.product_uom.id) / bom_id.product_qty
-                res = bom_id._bom_explode(line.product_id, factor, property_ids)
-    
-                for bom_line in res[0]:
-                    product = prod_obj.browse( bom_line['product_id'])
-                    
-                    res = self.env['sale.order.line'].product_id_change(
-                                    line.order_id.pricelist_id.id, product.id,
-                                    qty=bom_line['product_qty'], uom=bom_line['product_uom'],
-                                    qty_uos=bom_line['product_qty'], uos=bom_line['product_uom'],
-                                    name=bom_line['name'], partner_id=line.order_id.partner_id.id, lang=False,
-                                    update_tax=True, date_order=line.order_id.date_order, packaging=False,
-                                    fiscal_position=line.order_id.fiscal_position.id, flag=False)
-                    
-                    valdef = res['value']
-                    
-                    
-                    valdef.update({
-                            'order_id': line.order_id.id,
-                            'product_id': product.id,
-                            'product_tmpl_id': product.product_tmpl_id.id,
-                            'product_uom': bom_line['product_uom'],
-                            'product_uom_qty': bom_line['product_qty'],
-    #                        'product_uos': line['product_uos'],
-    #                        'product_uos_qty': line['product_uos_qty'],
-                            'name': bom_line['name'],
-    #                        'discount':line.discount,
-                            'tax_id': [(4,tax_id, False) for tax_id in valdef['tax_id']]
-                        })
-                    
-                    sol_id = self.create(valdef)
-                    to_explode_again_ids |= sol_id
-                
-                
-                to_unlink_ids |= line
-                
-        #check if new moves needs to be exploded
-        if to_explode_again_ids:
-            to_explode_again_ids.explode_set_contents()   
-        #delete the line with original product which is not relevant anymore     
-        if to_unlink_ids:
-            to_unlink_ids.unlink()
+#     @api.multi
+#     def explode_set_contents(self):
+#         """ Explodes order lines.
+#         """
+#         
+#         bom_obj = self.env['mrp.bom'].sudo()
+#         prod_obj = self.env["product.product"].sudo()
+#         uom_obj = self.env["uom.uom"].sudo()
+#         to_unlink_ids = self.env['sale.order.line']
+#         to_explode_again_ids = self.env['sale.order.line']
+#             
+#         for line in self.filtered(lambda l: l.set_product == True):
+#             bom_id = bom_obj._bom_find(product=line.product_id)
+#             if not bom_id:
+#                 continue
+#             bom_id = bom_obj.browse(bom_id)
+#             if bom_id.type == 'phantom':
+#                 factor = uom_obj._compute_qty(line.product_uom.id, line.product_uom_qty, bom_id.product_uom.id) / bom_id.product_qty
+#                 res = bom_id._bom_explode(line.product_id, factor)
+#     
+#                 for bom_line in res[0]:
+#                     product = prod_obj.browse( bom_line['product_id'])
+#                     
+#                     res = self.env['sale.order.line'].product_id_change(
+#                                     line.order_id.pricelist_id.id, product.id,
+#                                     qty=bom_line['product_qty'], uom=bom_line['product_uom'],
+#                                     qty_uos=bom_line['product_qty'], uos=bom_line['product_uom'],
+#                                     name=bom_line['name'], partner_id=line.order_id.partner_id.id, lang=False,
+#                                     update_tax=True, date_order=line.order_id.date_order, packaging=False,
+#                                     fiscal_position=line.order_id.fiscal_position.id, flag=False)
+#                     
+#                     valdef = res['value']
+#                     
+#                     
+#                     valdef.update({
+#                             'order_id': line.order_id.id,
+#                             'product_id': product.id,
+#                             'product_tmpl_id': product.product_tmpl_id.id,
+#                             'product_uom': bom_line['product_uom'],
+#                             'product_uom_qty': bom_line['product_qty'],
+#     #                        'product_uos': line['product_uos'],
+#     #                        'product_uos_qty': line['product_uos_qty'],
+#                             'name': bom_line['name'],
+#     #                        'discount':line.discount,
+#                             'tax_id': [(4,tax_id, False) for tax_id in valdef['tax_id']]
+#                         })
+#                     
+#                     sol_id = self.create(valdef)
+#                     to_explode_again_ids |= sol_id
+#                 
+#                 
+#                 to_unlink_ids |= line
+#                 
+#         #check if new moves needs to be exploded
+#         if to_explode_again_ids:
+#             to_explode_again_ids.explode_set_contents()   
+#         #delete the line with original product which is not relevant anymore     
+#         if to_unlink_ids:
+#             to_unlink_ids.unlink()
             
 
     
