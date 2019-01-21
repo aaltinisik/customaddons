536d2423cb7af3caa544ae68bd938c076a5c7f16
onurugur <onurugur@gitlab.com>
2019-01-21 13:46:08 +0300

    vat modul

--- l10n_tr_vat/models/partner.py
+++ l10n_tr_vat/models/partner.py
@@ -12,18 +12,19 @@ class ResPartner(models.Model):
     @api.constrains('vat')
     def check_vat(self):
         for partner in self:
-            if partner.sanitized_vat == '11111111111' or partner.sanitized_vat == '2222222222' :
+            if partner.vat == '11111111111' or partner.vat == '2222222222' :
                 return False
             if not partner.vat or partner.vat == "False":
                 return False
             if partner.parent_id:
                 return False
-            colliding_partner = self.search([('sanitized_vat', '=', partner.sanitized_vat),('id', '!=', partner.id),('is_company', '=', 1)],limit=1)
+            colliding_partner = self.search([('vat', '=', partner.vat),('id', '!=', partner.id),('is_company', '=', 1)],limit=1)
             colliding_partner = self.browse(colliding_partner.id)
             if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
                 if colliding_partner.commercial_partner_id != partner.commercial_partner_id:
                     raise UserError(_('The VAT number [%s] for this partner is also registered with partner [%s] Each partner should have unique vat number.') % (colliding_partner[0].vat,colliding_partner[0].name))
             else:
+                #TDE Fix false return should raise
                 self.check_vat_tr(partner.vat)
         return super(ResPartner,self).check_vat()
 
@@ -32,11 +33,15 @@ class ResPartner(models.Model):
     @api.multi
     def write(self,values):
         if 'vat' in values:
-            self.check_vat()
+            if values['vat']:
+                values['vat'] = re.sub(r'\W+', '', values['vat']).upper()
+              #  self.check_vat()
         return super(ResPartner,self).write(values)
     
     @api.model
     def create(self,values):
         if 'vat' in values:
-            self.check_vat()
+            if values['vat']:
+                values['vat'] = re.sub(r'\W+', '', values['vat']).upper()
+            #self.check_vat()
         return super(ResPartner,self).create(values)
