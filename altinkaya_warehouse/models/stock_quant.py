51c98eb3764489d2308bfbd99d44b6c289a36fe0
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2019-02-19 07:31:19 +0300

    removed stock quant ignore reservation feature.

--- altinkaya_warehouse/models/stock_quant.py
+++ altinkaya_warehouse/models/stock_quant.py
@@ -15,22 +15,22 @@ class stock_quant(models.Model):
     priority = fields.Integer(related='location_id.priority', help='high priority quants will be reserved first',
                              readonly=True, store=True)
 
-    @api.model
-    def _quants_get_order(self, location, product, quantity, domain=[], orderby='in_date'):
-        ''' overwrite default behavior
-        '''
-        def check_ignore_disable(domain):
-
-            for node in domain:
-                if isinstance(node,tuple):
-                    if node[0] == 'history_ids':
-                        return True
-            return False
-
-
-        if  not check_ignore_disable(domain) and location and not location.ignore_reservation:
-                domain += [('ignore_reservation','=',False)]
-
-        return super(stock_quant, self)._quants_get_order(location=location, product=product, quantity=quantity,
-                                                                  domain=domain, orderby='priority, %s' % orderby)
-    
+    # @api.model
+    # def _quants_get_order(self, location, product, quantity, domain=[], orderby='in_date'):
+    #     ''' overwrite default behavior
+    #     '''
+    #     def check_ignore_disable(domain):
+    #
+    #         for node in domain:
+    #             if isinstance(node,tuple):
+    #                 if node[0] == 'history_ids':
+    #                     return True
+    #         return False
+    #
+    #
+    #     if  not check_ignore_disable(domain) and location and not location.ignore_reservation:
+    #             domain += [('ignore_reservation','=',False)]
+    #
+    #     return super(stock_quant, self)._quants_get_order(location=location, product=product, quantity=quantity,
+    #                                                               domain=domain, orderby=orderby)
+    #
