b54f5ae467aa16ad9e90130d76aea63db6876f87
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2019-04-18 16:46:13 +0300

    do not count negative virtual qty on secondary location on orderpoint virtual available creation

--- altinkaya_warehouse/models/procurement.py
+++ altinkaya_warehouse/models/procurement.py
@@ -22,9 +22,11 @@ class ProcurementOrder(models.Model):
     @api.model
     def _product_virtual_get(self, order_point):
         res = super(ProcurementOrder, self)._product_virtual_get(order_point)
-        
+
         other_res = order_point.product_id.with_context({'location':order_point.location_id.stock_location_ids.ids}).virtual_available
         
+        if other_res < 0:
+            other_res = 0
         
         return res + other_res
     
