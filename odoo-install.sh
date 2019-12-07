675f04e2d932580e85641736a5c3b539f59f5856
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2019-12-07 12:42:49 +0300

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -94,7 +94,7 @@ echo -e "\n---- Install PostgreSQL Server ----"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-11  postgresql-contrib -y
+sudo apt-get install postgresql-11 -y
 
 #--------------------------------------------------
 # Install Dependencies
