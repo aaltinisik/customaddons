8f8d9059e99d8d3ebd5a0e04dd8c814d9b7ba5a7
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2019-12-07 14:08:31 +0300

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -80,6 +80,7 @@ echo -e "\n---- Update Server ----"
 # universe package is for Ubuntu 18.x
 sudo add-apt-repository universe
 # libpng12-0 dependency for wkhtmltopdf
+sudo add-apt-repository "deb http://mirrors.kernel.org/ubuntu/ xenial main"
 sudo apt-get update
 sudo apt-get upgrade -y
 
@@ -97,8 +98,8 @@ sudo apt-get install postgresql-11 -y
 # Install Dependencies
 #--------------------------------------------------
 echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install git python3 python3-pip build-essential wget mc python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libpng12-0 gdebi -y
-
+sudo apt-get install git python3 python3-pip build-essential wget mc python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less gdebi -y
+sudo apt-get install libpng12-0 -y
 echo -e "\n---- Install python packages/requirements ----"
 sudo pip3 install -r https://github.com/odoo/odoo/raw/$OE_VERSION/requirements.txt
 
