febe9330d9bc34feb88ee4801a81229500eb51c5
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2020-01-09 16:24:26 +0300

    added upstream remotes to install script

--- install-customaddons.sh
+++ install-customaddons.sh
@@ -16,33 +16,144 @@ sudo su $OE_USER -c "mkdir -p -v $OE_HOMEV/addons"
 cd $OE_HOMEV/repos
 
 git clone -b $OE_VERSION https://github.com/aaltinisik/access-addons
+cd access-addons
+git remote add upstream https://github.com/it-projects-llc/access-addons.git
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/account-financial-tools
+cd account-financial-tools
+git remote add upstream https://github.com/OCA/account-financial-tools.git
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/account-invoicing.git
+cd account-invoicing 
+git remote add upstream https://github.com/OCA/account-invoicing
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/account-payment.git
+cd  account-payment
+git remote add upstream https://github.com/OCA/account-payment
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/connector-telephony.git
+cd  connector-telephony
+git remote add upstream https://github.com/OCA/connector-telephony
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/crm
+cd  crm
+git remote add upstream https://github.com/OCA/crm
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/knowledge.git
+cd  knowledge
+git remote add upstream https://github.com/OCA/knowledge
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/misc-addons.git
+cd  misc-addons
+git remote add upstream https://github.com/it-projects-llc/misc-addons 
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/partner-contact.git
+cd  partner-contact
+git remote add upstream https://github.com/OCA/partner-contact
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/product-attribute.git
+cd  product-attribute
+git remote add upstream https://github.com/OCA/product-attribute
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/purchase-workflow.git
+cd  purchase-workflow
+git remote add upstream  https://github.com/OCA/purchase-workflow
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/reporting-engine
+cd  reporting-engine
+git remote add upstream https://github.com/OCA/reporting-engine
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/report-print-send.git
+cd  report-print-send
+git remote add upstream https://github.com/OCA/report-print-send
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/sale-workflow
+cd  sale-workflow
+git remote add upstream https://github.com/OCA/sale-workflow
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/server-tools.git
+cd  server-tools
+git remote add upstream https://github.com/OCA/server-tools
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/stock-logistics-tracking.git
+cd  stock-logistics-tracking
+git remote add upstream https://github.com/OCA/stock-logistics-tracking
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/stock-logistics-warehouse
+cd stock-logistics-warehouse
+git remote add upstream https://github.com/OCA/stock-logistics-warehouse
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/stock-logistics-workflow
+cd stock-logistics-workflow
+git remote add upstream https://github.com/OCA/stock-logistics-workflow
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/web.git
+cd web
+git remote add upstream  https://github.com/OCA/web
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/CybroAddons.git
+cd CybroAddons
+git remote add upstream https://github.com/CybroOdoo/CybroAddons
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/addons-vauxoo.git
+cd addons-vauxoo
+git remote add upstream https://github.com/Vauxoo/addons-vauxoo
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/SerpentCS_Contributions.git
+cd SerpentCS_Contributions
+git remote add upstream https://github.com/JayVora-SerpentCS/SerpentCS_Contributions
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/stock-logistics-reporting.git
+cd stock-logistics-reporting
+git remote add upstream https://github.com/OCA/stock-logistics-reporting
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/manufacture.git
+cd manufacture
+git remote add upstream https://github.com/OCA/manufacture
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/odoomrp-wip.git
+cd odoomrp-wip
+git remote add upstream https://github.com/odoomrp/odoomrp-wip
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/mail-addons.git
+cd mail-addons
+git remote add upstream https://github.com/it-projects-llc/mail-addons
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/stock-logistics-barcode.git
+cd  stock-logistics-barcode
+git remote add upstream https://github.com/OCA/stock-logistics-barcode
+cd ..
+
 git clone -b $OE_VERSION https://github.com/aaltinisik/techspawn-odoo-apps.git
+cd  techspawn-odoo-apps
+git remote add upstream https://github.com/techspawn/odoo-apps
+cd ..
 
 
 echo -e "\n---- Setting permissions on home folder ----"
