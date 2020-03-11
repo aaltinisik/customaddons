656cb3427dc7dfe08c64a81004b5713934637035
root <root@erp.altinkaya.com.tr>
2020-03-11 18:11:52 +0300

    added address models to the migration script

--- altinkaya_migration_v8_to_v12/models/model.py
+++ altinkaya_migration_v8_to_v12/models/model.py
@@ -22,5 +22,21 @@ class Rescountry(models.Model):
     _name = 'res.country'
     _inherit= ["res.country",'migration.mapping'] 
     
+class RescountryState(models.Model):
+    _name = 'res.country.state'
+    _inherit= ["res.country.state",'migration.mapping']
 
-    
+class AddressDistrict(models.Model):
+    _name = 'address.district'
+    _inherit= ["address.district",'migration.mapping']
+
+class AddressRegion(models.Model):
+    _name = 'address.region'
+    _inherit= ["address.region",'migration.mapping']
+
+class AddressNeighbour(models.Model):
+    _name = 'address.neighbour'
+    _inherit= ["address.neighbour",'migration.mapping']
+
+
+
