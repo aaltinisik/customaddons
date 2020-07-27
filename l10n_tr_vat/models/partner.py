17076e5c3eec523ed9b86ebeb02d40265c1704db
Mehmet Akif Özdemir <ozdemir.mhmtakif@gmail.com>
2020-07-27 22:52:59 +0300

    disable vat check during migration

--- l10n_tr_vat/models/partner.py
+++ l10n_tr_vat/models/partner.py
@@ -11,22 +11,23 @@ class ResPartner(models.Model):
     
     @api.constrains('vat')
     def check_vat(self):
-        for partner in self:
-            if partner.vat == '11111111111' or partner.vat == '2222222222' :
-                return False
-            if not partner.vat or partner.vat == "False":
-                return False
-            if partner.parent_id:
-                return False
-            colliding_partner = self.search([('vat', '=', partner.vat),('id', '!=', partner.id),('is_company', '=', 1)],limit=1)
-            colliding_partner = self.browse(colliding_partner.id)
-            if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
-                if colliding_partner.commercial_partner_id != partner.commercial_partner_id:
-                    raise UserError(_('The VAT number [%s] for this partner is also registered with partner [%s] Each partner should have unique vat number.') % (colliding_partner[0].vat,colliding_partner[0].name))
-            else:
-                #TDE Fix false return should raise
-                self.check_vat_tr(partner.vat)
-        return super(ResPartner,self).check_vat()
+        return True
+#         for partner in self:
+#             if partner.vat == '11111111111' or partner.vat == '2222222222' :
+#                 return False
+#             if not partner.vat or partner.vat == "False":
+#                 return False
+#             if partner.parent_id:
+#                 return False
+#             colliding_partner = self.search([('vat', '=', partner.vat),('id', '!=', partner.id),('is_company', '=', 1)],limit=1)
+#             colliding_partner = self.browse(colliding_partner.id)
+#             if colliding_partner and colliding_partner.company_id.id == partner.company_id.id:
+#                 if colliding_partner.commercial_partner_id != partner.commercial_partner_id:
+#                     raise UserError(_('The VAT number [%s] for this partner is also registered with partner [%s] Each partner should have unique vat number.') % (colliding_partner[0].vat,colliding_partner[0].name))
+#             else:
+#                 #TDE Fix false return should raise
+#                 self.check_vat_tr(partner.vat)
+#         return super(ResPartner,self).check_vat()
 
 
 
