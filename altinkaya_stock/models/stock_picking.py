42fd195e926901876973ab30d6bfff74d0233723
yibudak <yibudak@gmail.com>
2021-05-21 16:11:57 +0300

    added new fields to stock.picking

--- altinkaya_stock/models/stock_picking.py
+++ altinkaya_stock/models/stock_picking.py
@@ -69,7 +69,9 @@ class StockPicking(models.Model):
     comment_irsaliye = fields.Text('İrsaliye Notu')
     hazirlayan = fields.Many2one('hr.employee', 'Sevki Hazırlayan')
     teslim_alan = fields.Char('Malı Teslim Alan Ad Soyad', size=32)
-    
+    teslimat_ucreti = fields.Float('Teslimat Ücreti')
+    desi = fields.Float('Desi')
+    koli_adedi = fields.Integer('Koli Adedi')    
     #TODO @dogan: invoice picking
 #     def _prepare_shipping_invoice_line(self, cr, uid, picking, invoice, context=None):
 #         res = super(stock_picking, self)._prepare_shipping_invoice_line(cr, uid, picking, invoice, context=context)
