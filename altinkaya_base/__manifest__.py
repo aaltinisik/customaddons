3eb81a4a6123cd0bfaa3a1e5d167f56b4b4e6085
Yavuz Avcı <yavuzavci@gmail.com>
2021-08-20 15:21:00 +0300

    Removed unnecessary files.

--- altinkaya_base/__manifest__.py
+++ altinkaya_base/__manifest__.py
@@ -1,35 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "altinkaya_base",
-
+    'name': "Altınkaya Custom View Updates",
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Includes inherited base views.
+        """,
+    'description': """        
     """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+    'author': "Yavuz Avcı",
+    'website': "https://www.altinkaya.com.tr/",
     'category': 'Uncategorized',
     'version': '0.1',
-
     # any module necessary for this one to work correctly
     'depends': ['base'],
-
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/open_module_tree.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    'installable': True
 }
