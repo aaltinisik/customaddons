1dab0516b782fe08ceb1268fefba5f67f312689b
Yavuz Avcı <yavuzavci@gmail.com>
2021-08-20 15:38:35 +0300

    removed unused files

--- altinkaya_base/__manifest__.py
+++ altinkaya_base/__manifest__.py
@@ -10,9 +10,9 @@
     'website': "https://www.altinkaya.com.tr/",
     'category': 'Uncategorized',
     'version': '0.1',
-    # any module necessary for this one to work correctly
+
     'depends': ['base'],
-    # always loaded
+
     'data': [
         # 'security/ir.model.access.csv',
         'views/open_module_tree.xml'
