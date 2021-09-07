d409a71ab8c95887cbabc39f7931690ca2409762
yibudak <yibudak@gmail.com>
2021-09-07 17:18:19 +0300

    fixed error while creating new record on account.invoice

--- account_invoice_export/models/account_invoice.py
+++ account_invoice_export/models/account_invoice.py
@@ -22,7 +22,7 @@ class AccountInvoice(models.Model):
         currency_obj = self.env['res.currency'].browse(self.currency_id.id)
         currency_try = self.env['res.currency'].search([('name', '=', 'TRY')], limit=1)
         company = self.env['res.company'].browse(1)
-        date = self.date_export or self.date_invoice
+        date = self.date_export or self.date_invoice or datetime.now()
         self.amount_total_tl = currency_obj._convert(self.amount_total, currency_try, company, date)
         self.amount_tax_tl = currency_obj._convert(self.amount_tax, currency_try, company, date)
         self.amount_untaxed_tl = currency_obj._convert(self.amount_untaxed, currency_try, company, date)
