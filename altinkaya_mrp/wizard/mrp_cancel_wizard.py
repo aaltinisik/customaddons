79e3e0bd3f290d833cef1abdfce4e188c8c27112
yibudak <yibudak@gmail.com>
2021-09-14 21:01:22 +0300

    new module for manual procurement

--- altinkaya_mrp/wizard/mrp_cancel_wizard.py
+++ altinkaya_mrp/wizard/mrp_cancel_wizard.py
@@ -6,10 +6,6 @@ class mrp_cancel_more(models.TransientModel):
     _name = 'mrp.cancel.more'
     _description="Mrp Cancel More"
 
-
-
-    #TODO: should check what is this doing onur
-
     @api.multi
     def cancel_mrp_order(self):
         """ Cancels the production order and related stock moves.
@@ -23,4 +19,37 @@ class mrp_cancel_more(models.TransientModel):
                 move_obj.action_cancel([x.id for x in production.move_finished_ids])
             move_obj.action_cancel( [x.id for x in production.move_raw_ids])
             production.write({'state': 'cancel'})
+        return True
+
+    @api.multi
+    def action_cancel(self):
+        """ Cancels production order, unfinished stock moves and set procurement
+        orders in exception """
+        if any(workorder.state == 'progress' for workorder in self.mapped('workorder_ids')):
+            raise UserError(_('You can not cancel production order, a work order is still in progress.'))
+        documents = {}
+        for production in self:
+            for move_raw_id in production.move_raw_ids.filtered(lambda m: m.state not in ('done', 'cancel')):
+                iterate_key = self._get_document_iterate_key(move_raw_id)
+                if iterate_key:
+                    document = self.env['stock.picking']._log_activity_get_documents({move_raw_id: (move_raw_id.product_uom_qty, 0)}, iterate_key, 'UP')
+                    for key, value in document.items():
+                        if documents.get(key):
+                            documents[key] += [value]
+                        else:
+                            documents[key] = [value]
+            production.workorder_ids.filtered(lambda x: x.state != 'cancel').action_cancel()
+            finish_moves = production.move_finished_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+            raw_moves = production.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+            (finish_moves | raw_moves)._action_cancel()
+            picking_ids = production.picking_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
+            picking_ids.action_cancel()
+        self.write({'state': 'cancel', 'is_locked': True})
+        if documents:
+            filtered_documents = {}
+            for (parent, responsible), rendering_context in documents.items():
+                if not parent or parent._name == 'stock.picking' and parent.state == 'cancel' or parent == self:
+                    continue
+                filtered_documents[(parent, responsible)] = rendering_context
+            self._log_manufacture_exception(filtered_documents, cancel=True)
         return True
