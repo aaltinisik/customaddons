86aa46ea33307af27922f8479a2bf11a63787d1b
yibudak <yibudak@gmail.com>
2021-11-16 17:29:46 +0300

    added new module for waybill printer

---
+++ stock_waybill_print/models/stock_warehouse.py
@@ -0,0 +1,32 @@
+from odoo import models, fields, api
+
+
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    waybill_sequence = fields.Char(string='Waybill Sequence')
+    waybill_number = fields.Integer('Waybill Number')
+    waybill_printer = fields.Many2one(
+       'printing.printer',
+       string=u'İrsaliye Yazıcısı',
+       help="",
+       required=False,
+       company_dependent=True)
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    document_number = fields.Char(string='İrsaliye Numarası', help='Basılı irsaliye numarası')
+    document_page_count = fields.Integer('Page Count', compute='_compute_page_count')
+
+    @api.one
+    def _compute_page_count(self):
+        if len(self.move_lines) % 24 != 0:
+            self.document_page_count = int(len(self.move_lines) / 24) + 1
+        else:
+            self.document_page_count = len(self.move_lines) / 24
+
+    @api.multi
+    def action_print(self):
+        return {}
