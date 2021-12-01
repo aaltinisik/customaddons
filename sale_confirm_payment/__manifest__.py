66c60a8f7a57faec74f055efb71b6467150734ae
yibudak <yibudak@gmail.com>
2021-12-01 17:40:10 +0300

    new module for confirm payment on sale.order

---
+++ sale_confirm_payment/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Sale Payment",
+    "summary": "Payment button in sale order",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "yibudak, Terrabit",
+    "website": "https://altinkaya.com.tr",
+    "depends": ["sale", "payment"],
+    "license": "LGPL-3",
+    "data": ["views/sale_view.xml", "wizard/sale_confirm_payment_view.xml"],
+    "maintainers": ["yibudak"],
+}
---
+++ sale_confirm_payment/models/sale.py
@@ -0,0 +1,54 @@
+from odoo import api, fields, models
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    acquirer_id = fields.Many2one("payment.acquirer", related="transaction_ids.acquirer_id", store=True)
+    payment_amount = fields.Monetary(string="Amount Payment", compute="_compute_payment")
+
+    payment_status = fields.Selection(
+        [
+            ("without", "Without"),
+            ("initiated", "Initiated"),
+            ("authorized", "Authorized"),
+            ("partial", "Partial"),
+            ("done", "Done"),
+        ],
+        default="without",
+        compute="_compute_payment",
+        store=True,
+    )
+
+    @api.multi
+    def action_confirm_payment(self):
+        aw_obj = self.env['ir.actions.act_window']
+        action = aw_obj.for_xml_id('sale_confirm_payment', 'action_confirm_payment_sale')
+        return action
+
+    @api.depends("transaction_ids")
+    def _compute_payment(self):
+        for order in self:
+
+            amount = 0
+            transactions = order.sudo().transaction_ids.filtered(lambda a: a.state == "done")
+            for invoice in order.invoice_ids:
+                amount += invoice.amount_total - invoice.residual
+                transactions = transactions - invoice.transaction_ids
+            for transaction in transactions:
+                amount += transaction.amount
+            order.payment_amount = amount
+            if amount:
+                if amount < order.amount_total:
+                    order.payment_status = "partial"
+                else:
+                    order.payment_status = "done"
+
+            if not amount:
+                order.payment_status = "without"
+                if order.transaction_ids:
+                    order.payment_status = "initiated"
+                    authorized_transaction_ids = order.transaction_ids.filtered(lambda t: t.state == "authorized")
+                    if authorized_transaction_ids:
+                        order.payment_status = "authorized"
+
