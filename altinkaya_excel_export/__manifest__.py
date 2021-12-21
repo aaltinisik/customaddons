42daee8081d9cd359f70b3d8e26072b06b9062da
yibudak <yibudak@gmail.com>
2021-12-21 15:35:01 +0300

    new module for excel export

---
+++ altinkaya_excel_export/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Altinkaya Excel Reports",
+    'summary': """
+        Various Excel reports""",
+    'description': """
+        This module will allow exporting in Excel using
+        OCA/server-tools/excel_import_export module.
+    """,
+    'author': "Yavuz Avcı,Yiğit Budak",
+    'website': "",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['excel_import_export', 'purchase', 'account'],
+    'data': [
+        # Purchase
+        'export_purchase_order_xlsx/reports.xml',
+        'export_purchase_order_xlsx/temp_po_en.xml',
+        'export_purchase_order_xlsx/temp_po_tr.xml',
+        'export_purchase_order_xlsx/temp_rfq_en.xml',
+        'export_purchase_order_xlsx/temp_rfq_tr.xml',
+        # Account
+        'export_account_invoice_xlsx/temp_zirve_masraf_fatura.xml',
+        'export_account_invoice_xlsx/reports.xml',
+    ],
+    'installable': True,
+}
