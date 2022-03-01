90369ad8c68c8842c7b90029e9d40419d1882be7
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2022-03-01 11:41:03 +0300

    removed __unported

--- __unported__/currency_multi_rate_update/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-'''
-Created on May 28, 2017
-
-@author: codequarters_ugur
-'''
-from . import models
--- __unported__/procurement_mass_manage/wizard/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-
-import run_procurement
-import check_procurement
-import reconfirm_procurement
-import cancel_procurement
