3798efc7d7d919bfd2753eddc5c02a722ad7d92a
yibudak <yibudak@gmail.com>
2022-03-11 15:56:20 +0300

    account.move.line create inherited

--- currency_difference_invoice/models/account_move_line.py
+++ currency_difference_invoice/models/account_move_line.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.tools import float_is_zero
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     is_difference_invoice = fields.Boolean(string='Is Difference Invoice')
     difference_amount = fields.Monetary(string='Difference Amount')
+
+    def create(self, vals):
+        res = super(AccountMoveLine, self).create(vals)
+        res.amount_currency = res._calculate_amount_currency()
+        return res
+
+    def _calculate_amount_currency(self):
+        prec = self.env['decimal.precision'].precision_get('Account')
+        for line in self:
+            partner_currency_id = line.partner_id.secondary_curr_id
+            if float_is_zero(line.amount_currency, prec) and partner_currency_id:
+                amount = line.credit or line.debit or 0.0
+                currency_id = line.currency_id or line.company_id.currency_id
+                return currency_id._convert(amount, partner_currency_id, line.company_id, line.date,
+                                                            round=False)
+            return line.amount_currency
--- currency_difference_invoice/models/res_partner.py
+++ currency_difference_invoice/models/res_partner.py
@@ -12,4 +12,10 @@ class ResPartner(models.Model):
     secondary_curr_id = fields.Many2one('res.currency', string='Currency')
 
     def calc_difference_invoice(self):
-        pass
+        prec = self.env['decimal.precision'].precision_get('Account')
+        for partner in self:
+            if partner.has_secondary_curr:
+                lines = self.env['account.move.line'].search([('partner_id', '=', partner.id), ('invoice_id', '!=', False)])
+                for line in lines:
+                    payment = line.credit or line.debit
+                    amount_currency = round(abs(line.amount_currency or line._calculate_amount_currency()), prec)
