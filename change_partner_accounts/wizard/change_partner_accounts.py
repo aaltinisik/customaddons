7c3623cb9bac7011eae738363ad095b2a74c1714
yibudak <yibudak@gmail.com>
2022-03-29 14:54:45 +0300

    added a method to convert amount currency

--- change_partner_accounts/wizard/change_partner_accounts.py
+++ change_partner_accounts/wizard/change_partner_accounts.py
@@ -23,14 +23,18 @@ class ChangePartnerAccounts(models.TransientModel):
 
         old_receivable_id = self.partner_id.property_account_receivable_id.id
         old_payable_id = self.partner_id.property_account_payable_id.id
+
+        if (old_payable_id == self.account_payable_id.id) or (old_receivable_id == self.account_receivable_id.id):
+            raise UserError(_('Accounts must be different than the current ones'))
+
         cr = self.env.cr
-        cr.execute("""update account_move_line line set account_id = t.account_id
+        cr.execute("""update account_move_line line set account_id = t.new_account_id
                     from (select ml.id as line_id, ml.account_id, ml.partner_id,           
                     case
                     when ml.account_id = {0} then {1} --- receivable
                     when ml.account_id = {2} then {3} --- payable
                     else ml.account_id
-                    end as account_id
+                    end as new_account_id
             
                     from account_move_line as ml 
                     where ml.partner_id = {4})
@@ -40,8 +44,46 @@ class ChangePartnerAccounts(models.TransientModel):
                                                           self.account_payable_id.id,
                                                           self.partner_id.id))
 
-        self.partner_id.write(value={'property_account_receivable_id': self.account_receivable_id.id,
-                                     'property_account_payable_id': self.account_payable_id.id})
+        self.partner_id.write({'property_account_receivable_id': self.account_receivable_id.id,
+                               'property_account_payable_id': self.account_payable_id.id})
 
-        # 3. account.move.line'larda amount_currencysi olmayan veya yeni hesaba uymayanları düzelteceğiz
+        self.calculate_amount_currency_on_partner_amls()
         return {"type": "ir.actions.act_window_close"}
+
+    @api.one
+    def calculate_amount_currency_on_partner_amls(self):
+        cr = self.env.cr
+        company_currency = self.env.user.company_id.currency_id
+        currency_id = (self.partner_id.property_account_receivable_id.currency_id and
+                       self.partner_id.property_account_receivable_id.currency_id) or \
+                      False
+
+        partner_amls = self.env['account.move.line'].search([('partner_id', '=', self.partner_id.id),
+                                                             ('account_id', 'in',
+                                                              [self.partner_id.property_account_receivable_id.id,
+                                                               self.partner_id.property_account_payable_id.id]),
+                                                             ('currency_id', '=', False)])
+
+        for aml in partner_amls:
+            amount_currency = company_currency._convert(aml.debit - aml.credit,
+                                                        currency_id,
+                                                        self.env.user.company_id, aml.date)
+
+            amount_residual_currency = company_currency._convert(aml.amount_residual,
+                                                                 currency_id,
+                                                                 self.env.user.company_id, aml.date)
+
+            cr.execute(
+                """ update account_move_line
+                
+                 set
+                  amount_currency = {0},
+                  currency_id = {1},
+                  amount_residual_currency = {2}
+                
+                where id = {3}""".format(amount_currency,
+                                         currency_id.id,
+                                         amount_residual_currency,
+                                         aml.id))
+
+        cr.commit()
