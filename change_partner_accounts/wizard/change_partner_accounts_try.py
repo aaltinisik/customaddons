3c402b28edbd4475c8e19c45a6ecc1f9f4e8aec8
yibudak <yibudak@gmail.com>
2022-04-08 15:23:09 +0300

    compute partner_currency field on account change

--- change_partner_accounts/wizard/change_partner_accounts_try.py
+++ change_partner_accounts/wizard/change_partner_accounts_try.py
@@ -13,6 +13,7 @@ class ChangePartnerAccountsTRY(models.TransientModel):
         for record in self.web_progress_iter(partners, msg="Müşterilerin hesapları değiştiriliyor..."):
             try:
                 record.change_accounts_to_try()
+                record._get_partner_currency()
                 self.env.cr.commit()
             except:
                 errors.append(record.display_name)
