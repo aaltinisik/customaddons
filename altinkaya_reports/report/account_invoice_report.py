a36a3c7a92b57cab3ee3f9e178b2b6750e37aacd
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2022-04-11 17:34:49 +0300

    added new fields utm source_id utm campaign_d

--- altinkaya_reports/report/account_invoice_report.py
+++ altinkaya_reports/report/account_invoice_report.py
@@ -24,14 +24,17 @@ class AccountInvoiceReport(models.Model):
     state_id = fields.Many2one('res.country.state', string='State', readonly=True)
     price_total_usd = fields.Float(string='Untaxed Total USD', readonly=True)
     price_average_usd = fields.Float(string='Average Price USD', readonly=True, group_operator="avg")
+    source_id = fields.Many2one('utm.source', string='Marketing Source', readonly=True)
+    campaign_id = fields.Many2one('utm.campaign', string='Marketing Campaign', readonly=True)
 
     def _select(self):
         return super(AccountInvoiceReport, self)._select() + \
-               ", sub.state_id, sub.price_total_usd as price_total_usd, sub.price_average_usd as price_average_usd"
+               ", sub.state_id, sub.price_total_usd as price_total_usd, sub.price_average_usd as price_average_usd, sub.source_id as source_id, sub.campaign_id as campaign_id"
 
     def _sub_select(self):
         return super(AccountInvoiceReport, self)._sub_select() + \
                """, coalesce(partner.state_id, partner_ai.state_id) AS state_id,
+               partner.source_id,partner.campaign_id,
                SUM(ail.price_subtotal_signed * invoice_type.sign * ai.usd_rate) AS price_total_usd,
                sum(abs(ail.price_subtotal_signed) * ai.usd_rate) /
                 CASE
@@ -40,5 +43,5 @@ class AccountInvoiceReport(models.Model):
                 END AS price_average_usd"""
 
     def _group_by(self):
-        return super(AccountInvoiceReport, self)._group_by() + ", coalesce(partner.state_id, partner_ai.state_id)"
+        return super(AccountInvoiceReport, self)._group_by() + ", coalesce(partner.state_id, partner_ai.state_id),partner.source_id,partner.campaign_id"
 
