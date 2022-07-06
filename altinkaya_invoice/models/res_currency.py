78d8324b01b350cc540692245554290a6ad6b56a
yiğit budak <yibudak@gmail.com>
2022-07-06 16:10:02 +0300

    [12.0][FIX] date fix on http method

--- altinkaya_invoice/models/res_currency.py
+++ altinkaya_invoice/models/res_currency.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, tools, _
-
+from datetime import datetime
 
 class ResCurrency(models.Model):
     _inherit = 'res.currency'
@@ -8,4 +8,4 @@ class ResCurrency(models.Model):
     def convert_currency_rate(self, from_amount, to_currency, company, date):
         to_currency_id = self.env['res.currency'].browse(to_currency)
         company_id = self.env['res.company'].browse(company)
-        return [to_currency_id.symbol, self._convert(from_amount, to_currency_id, company_id, date, True)]
+        return [to_currency_id.symbol, self._convert(from_amount, to_currency_id, company_id, datetime.strptime(date, '%d-%m-%Y'), True)]
