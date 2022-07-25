cd8d39e4baae5ab67a9ce5f02575eef3e1892ded
yiğit budak <yibudak@gmail.com>
2022-07-25 14:30:02 +0300

    [12.0][ADD] move_count field

--- partner_ranking/models/product_product.py
+++ partner_ranking/models/product_product.py
@@ -4,14 +4,16 @@ from datetime import timedelta
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, _
 import logging
+
 _logger = logging.getLogger(__name__)
+
 
 class Product(models.Model):
     _inherit = 'product.product'
 
-    sale_qty30days = fields.Float('Sale in last 30 days',readonly=True)
-    sale_qty180days = fields.Float('Sale in last 180 days',readonly=True)
-    sale_qty360days = fields.Float('Sale in last 360 days',readonly=True)
+    sale_qty30days = fields.Float('Sale in last 30 days', readonly=True)
+    sale_qty180days = fields.Float('Sale in last 180 days', readonly=True)
+    sale_qty360days = fields.Float('Sale in last 360 days', readonly=True)
 
     _defaults = {
         'sale_qty30days': 0.0,
@@ -26,7 +28,7 @@ class Product(models.Model):
         scheduler for sale count calculations.
         """
         _logger.info("\nScheduler started:: For product sales calculations......................")
-        prev_date =str((datetime.now() - timedelta(days=30)).date())
+        prev_date = str((datetime.now() - timedelta(days=30)).date())
         self._cr.execute('''WITH subquery AS 
                             (SELECT product_id,SUM(product_qty) as total_qty  FROM
                             account_invoice_report WHERE 
@@ -36,9 +38,9 @@ class Product(models.Model):
                             GROUP BY product_id)
                             UPDATE product_product SET sale_qty30days = subquery.total_qty
                             FROM subquery WHERE product_product.id=subquery.product_id
-                        ''' %(prev_date))
+                        ''' % (prev_date))
 
-        prev_date =str((datetime.now() - timedelta(days=180)).date())
+        prev_date = str((datetime.now() - timedelta(days=180)).date())
         self._cr.execute('''WITH subquery AS 
                             (SELECT product_id,SUM(product_qty) as total_qty  FROM
                             account_invoice_report WHERE 
@@ -48,8 +50,8 @@ class Product(models.Model):
                             GROUP BY product_id)
                             UPDATE product_product SET sale_qty180days = subquery.total_qty
                             FROM subquery WHERE product_product.id=subquery.product_id
-                        ''' %(prev_date))
-        prev_date =str((datetime.now() - timedelta(days=360)).date())
+                        ''' % (prev_date))
+        prev_date = str((datetime.now() - timedelta(days=360)).date())
         self._cr.execute('''WITH subquery AS 
                             (SELECT product_id,SUM(product_qty) as total_qty  FROM
                             account_invoice_report WHERE 
@@ -59,5 +61,25 @@ class Product(models.Model):
                             GROUP BY product_id)
                             UPDATE product_product SET sale_qty360days = subquery.total_qty
                             FROM subquery WHERE product_product.id=subquery.product_id
-                        ''' %(prev_date))
+                        ''' % (prev_date))
+
+        self._cr.execute('''
+                    WITH subquery AS (
+                      SELECT 
+                        product_id, 
+                        count(product_id) as id_count 
+                      FROM 
+                        stock_move 
+                      GROUP BY 
+                        product_id
+                    ) 
+                    UPDATE 
+                      product_product 
+                    SET 
+                      move_count = subquery.id_count 
+                    FROM 
+                      subquery 
+                    WHERE 
+                      product_product.id = subquery.product_id''')
+
         _logger.info("\nScheduler ended:: For product sales calculations......................")
