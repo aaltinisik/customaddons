e54484a57cf9b06fbb3b0fdc174ff6009a8be72d
Ahmet Altinisik <aaltinisik@altinkaya.com.tr>
2022-08-19 13:54:37 +0300

    [XML] changed report account payment template

--- altinkaya_invoice/models/account_move_line.py
+++ altinkaya_invoice/models/account_move_line.py
@@ -5,3 +5,4 @@ class AccountMoveLine(models.Model):
 
     move_name = fields.Char(related='move_id.name', string='Move Number')
     move_ref = fields.Char(related='move_id.ref', string='Move Reference')
+    journal_id = fields.Many2one('account.journal', related='move_id.journal_id', string='Journal')
