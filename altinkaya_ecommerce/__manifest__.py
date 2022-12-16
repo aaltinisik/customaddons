b6863c9805728a45263c0d45f3680532264af32c
yiğit budak <yibudak@gmail.com>
2022-12-16 13:54:04 +0300

    [12.0][ADD] altinkaya_ecommerce

---
+++ altinkaya_ecommerce/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Altinkaya E-commerce Extensions",
+    "summary": "Product and sale e-commerce fields",
+    "development_status": "Beta",
+    "version": "12.0.1.0.1",
+    "author": "Yiğit Budak",
+    "license": "AGPL-3",
+    "website": "https://github.com/yibudak",
+    "category": "Extensions",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        'views/product_template_view.xml',
+        'views/product_attribute_view.xml',
+    ],
+    'installable': True,
+
+}
