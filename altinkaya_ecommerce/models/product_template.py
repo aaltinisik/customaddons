eab9c079a28ccd9e6daa5492057bfb9fb92538d7
yiğit budak <yibudak@gmail.com>
2022-12-16 16:29:22 +0300

    [12.0][ADD] fill missing product attributes

--- altinkaya_ecommerce/models/product_template.py
+++ altinkaya_ecommerce/models/product_template.py
@@ -32,6 +32,7 @@ class ProductTemplate(models.Model):
     @api.model
     def _domain_website_attachment_ids(self):
         """Get domain for website attachments."""
+        #Todo fix domain with no attachments
         ctx_params = self.env.context.get('params', {})
         domain = []
 
@@ -54,3 +55,45 @@ class ProductTemplate(models.Model):
                                         " with single value. %s "
                                         % attr_line.attribute_id.display_name))
         return res
+
+    def action_fill_missing_product_attrs(self):
+        """Fill missing product variants for published attribute values."""
+        if len(self.product_variant_ids) == 1:
+            raise ValidationError(_("You can not fill missing product of non"
+                                    " variant product."))
+
+        tmpl_attribute_lines = self.attribute_line_ids.filtered(lambda x: x.required)
+        required_attrs = tmpl_attribute_lines.mapped('attribute_id')
+        filled_variant_ids = []
+
+        for product in self.product_variant_ids:
+            product_attrs = product.attribute_value_ids.mapped('attribute_id')
+            if not all(x in product_attrs.ids for x in required_attrs.ids):
+                missing_attrs = required_attrs - product_attrs
+                for attr in missing_attrs:
+                    tmpl_line = tmpl_attribute_lines.filtered(lambda l: l.attribute_id == attr)
+
+                    default_attr_value = attr.value_ids.filtered(lambda v: v.is_default)
+                    if not default_attr_value:
+                        raise ValidationError(_(
+                            "Set default value for attribute %s"
+                            % attr.name
+                        ))
+                    product.attribute_value_ids |= default_attr_value
+                    tmpl_line.value_ids |= default_attr_value
+                    filled_variant_ids.append(product.id)
+
+        if not filled_variant_ids:
+            raise ValidationError(_("No missing product variants found."))
+
+
+        tree_view_id = self.env.ref('product.product_product_tree_view').id
+        action = {
+            'type': 'ir.actions.act_window',
+            'views': [(tree_view_id, 'tree')],
+            'view_mode': 'tree,form',
+            'name': _('Products'),
+            'res_model': 'product.product',
+            'domain': "[('type', '=', 'product'), ('id', 'in', %s)]" % filled_variant_ids,
+        }
+        return action
