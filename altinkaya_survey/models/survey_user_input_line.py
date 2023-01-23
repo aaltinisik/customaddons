1ab173c3dc1f9cb6fdbea41309b26f3da4507fc2
yiğit budak <yibudak@gmail.com>
2023-01-23 18:03:57 +0300

    [12.0][FIX] added general_answer query

--- altinkaya_survey/models/survey_user_input_line.py
+++ altinkaya_survey/models/survey_user_input_line.py
@@ -6,8 +6,22 @@ from odoo import api, fields, models
 class SurveyUserInputLine(models.Model):
     _inherit = "survey.user_input_line"
 
-    partner_id = fields.Many2one(related='user_input_id.partner_id',
-                                 store=True)
+    partner_id = fields.Many2one(related="user_input_id.partner_id", store=True)
+
+    general_answer = fields.Char(
+        compute="_compute_general_answer",
+    )
+
+    @api.multi
+    def _compute_general_answer(self):
+        for record in self:
+            if record.question_id.type == "simple_choice":
+                record.general_answer = record.value_suggested.value
+
+            else:
+                answer_field = record.value_free_text or record.value_number
+                record.general_answer = str(answer_field)
+        return True
 
     @api.model
     def save_line_star_rating(self, user_input_id, question, post, answer_tag):
@@ -18,7 +32,9 @@ class SurveyUserInputLine(models.Model):
             "skipped": False,
         }
         if answer_tag in post and post[answer_tag].strip():
-            vals.update({"answer_type": "number", "value_number": int(post[answer_tag])})
+            vals.update(
+                {"answer_type": "number", "value_number": int(post[answer_tag])}
+            )
         else:
             vals.update({"answer_type": None, "skipped": True})
         old_uil = self.search(
