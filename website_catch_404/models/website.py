3ecdfde5e97368412e209c69b114060a12982999
yiğit budak <yibudak@gmail.com>
2023-04-10 11:28:10 +0300

    [16.0][FIX] URL method replaced

--- website_catch_404/models/website.py
+++ website_catch_404/models/website.py
@@ -1,6 +1,25 @@
 # Copyright 2023 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import models, fields
+from urllib.parse import urlparse, urlunparse
+
+
+def remove_domain_and_protocol(url):
+    """
+    Removes domain and protocol from the url.
+    """
+    parsed_url = urlparse(url)
+    modified_url = urlunparse(
+        (
+            "",
+            "",
+            parsed_url.path,
+            parsed_url.params,
+            parsed_url.query,
+            parsed_url.fragment,
+        )
+    )
+    return modified_url
 
 
 class Website(models.Model):
@@ -14,8 +33,7 @@ class Website(models.Model):
     )
 
     def _catch_404_error(self, request):
-        full_url = request.url
-        url = full_url.replace(self.domain, "")  # Remove domain from url
+        url = remove_domain_and_protocol(request.url)
         request_method = request.method
         website_id = self.id
         error = (
