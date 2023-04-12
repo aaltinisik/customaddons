94c9d486a5c7c113d1104fab1d9345000ff298ba
yiğit budak <yibudak@gmail.com>
2023-04-12 11:26:32 +0300

    [16.0][FIX] Show full products to internal user

--- website_set_product/controllers/main.py
+++ website_set_product/controllers/main.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo.addons.website_sale.controllers.main import WebsiteSale
+from odoo.http import request
 
 
 class WebsiteSaleInherit(WebsiteSale):
@@ -9,7 +10,10 @@ class WebsiteSaleInherit(WebsiteSale):
             WebsiteSaleInherit, self
         )._shop_lookup_products(attrib_set, options, post, search, website)
 
-        if search_result:
+        # Don't show set products in search results.
+        # If the request user is internal user, don't filter set products.
+
+        if search_result and not request.env.user.has_group("base.group_user"):
             search_result = search_result.filtered(lambda p: not p.sub_component)
             product_count = len(search_result)
 
