a633bbea6b61134424997f005ac52f94c5572e74
Yiğit Budak <yibudak@gmail.com>
2023-05-15 18:46:25 +0300

    [12.0][FIX] Second rate removed

--- altinkaya_invoice/models/res_currency_rate.py
+++ altinkaya_invoice/models/res_currency_rate.py
@@ -4,14 +4,15 @@ from odoo import api, fields, models, tools, _
 class CurrencyRate(models.Model):
     _inherit = "res.currency.rate"
 
-    rate_inverse = fields.Float(digits=(12, 6), default=1.0, compute="_compute_rate_inverse",
-                                help="The inverse rate of the currency")
-    rate_inverse_second = fields.Float(digits=(12, 6), default=1.0, compute="_compute_rate_inverse",
-                                       help="The inverse rate of the currency")
+    rate_inverse = fields.Float(
+        digits=(12, 6),
+        default=1.0,
+        compute="_compute_rate_inverse",
+        help="The inverse rate of the currency",
+    )
 
     @api.multi
-    @api.depends('rate', 'second_rate')
+    @api.depends("rate")
     def _compute_rate_inverse(self):
         for rate in self:
             rate.rate_inverse = 1.0 / rate.rate
-            rate.rate_inverse_second = 1.0 / rate.second_rate
