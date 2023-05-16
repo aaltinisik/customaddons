75c6b0d04def947f7d8d0d109d3211b516c3b261
Yiğit Budak <yibudak@gmail.com>
2023-05-16 09:06:03 +0300

    [12.0][FIX] Second rate removed

--- currency_rate_turkey/models/account_payment.py
+++ currency_rate_turkey/models/account_payment.py
@@ -11,10 +11,10 @@ class AccountPayment(models.Model):
     @api.multi
     def post(self):
         new_context = self._context.copy()
-        if self.partner_id.property_rate_type != "rate":
+        if self.partner_id.property_rate_field != "rate":
             new_context.update(
                 {
-                    "rate_type": self.partner_id.property_rate_type,
+                    "rate_type": self.partner_id.property_rate_field,
                 }
             )
         return super(AccountPayment, self.with_context(new_context)).post()
