ba21902c53816c56fffe4e78d67a44845119660f
yiğit budak <yibudak@gmail.com>
2023-07-21 17:58:49 +0300

    [12.0][FIX] sale.py black code refactor

--- sale_confirm_payment/models/sale.py
+++ sale_confirm_payment/models/sale.py
@@ -4,9 +4,11 @@ from odoo import api, fields, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    acquirer_id = fields.Many2one("payment.acquirer", related="transaction_ids.acquirer_id", store=True)
+    acquirer_id = fields.Many2one(
+        "payment.acquirer", related="transaction_ids.acquirer_id", store=True
+    )
     payment_amount = fields.Monetary(string="Amount Payment", readonly=True)
-    payment_ids = fields.Many2many('account.payment', string='Payments', readonly=True)
+    payment_ids = fields.Many2many("account.payment", string="Payments", readonly=True)
 
     payment_status = fields.Selection(
         [
@@ -23,15 +25,19 @@ class SaleOrder(models.Model):
 
     @api.multi
     def action_confirm_payment(self):
-        aw_obj = self.env['ir.actions.act_window']
-        action = aw_obj.for_xml_id('sale_confirm_payment', 'action_confirm_payment_sale')
+        aw_obj = self.env["ir.actions.act_window"]
+        action = aw_obj.for_xml_id(
+            "sale_confirm_payment", "action_confirm_payment_sale"
+        )
         return action
 
     @api.depends("transaction_ids")
     def _compute_payment_state(self):
         for order in self:
             amount = 0
-            transactions = order.sudo().transaction_ids.filtered(lambda a: a.state == "done")
+            transactions = order.sudo().transaction_ids.filtered(
+                lambda a: a.state == "done"
+            )
             for invoice in order.invoice_ids:
                 amount += invoice.amount_total - invoice.residual
                 transactions = transactions - invoice.transaction_ids
@@ -48,7 +54,8 @@ class SaleOrder(models.Model):
                 order.payment_status = "without"
                 if order.transaction_ids:
                     order.payment_status = "initiated"
-                    authorized_transaction_ids = order.transaction_ids.filtered(lambda t: t.state == "authorized")
+                    authorized_transaction_ids = order.transaction_ids.filtered(
+                        lambda t: t.state == "authorized"
+                    )
                     if authorized_transaction_ids:
                         order.payment_status = "authorized"
-
