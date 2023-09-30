f2b5de5f578c271ec7098b82c29388a3eb6cc272
yibudak <yibudak@gmail.com>
2023-10-01 01:18:20 +0300

    [FIX] website_debug_restrict: hide all the messages

--- website_debug_restrict/models/http.py
+++ website_debug_restrict/models/http.py
@@ -1,30 +1,25 @@
 # Copyright 2023 Yiğit Budak (https://github.com/yibudak)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 from odoo import http
-import traceback
-from odoo.tools import ustr
 
 
-def serialize_exception(exception):
+def _response(self, result=None, error=None):
     """
     If the user is in the debug group, return the full traceback
     If not, return a restricted message
     """
-    name = type(exception).__name__
-    module = type(exception).__module__
-
-    if http.request.env.user.has_group("website_debug_restrict.restrict_debug_mode"):
-        debug_log = traceback.format_exc()
-    else:
-        debug_log = "Restricted"
-    return {
-        "name": f"{module}.{name}" if module else name,
-        "debug": debug_log,
-        "message": ustr(exception),
-        "arguments": exception.args,
-        "context": getattr(exception, "context", {}),
-    }
+    response = {"jsonrpc": "2.0", "id": self.request_id}
+    if error is not None:
+        if not self.request.env.user.has_group(
+            "website_debug_restrict.restrict_debug_mode"
+        ):
+            response["result"] = {"error": "Restricted."}
+        else:
+            response["error"] = error
+    if result is not None:
+        response["result"] = result
+    return self.request.make_json_response(response)
 
 
 # Monkey patching
-http.serialize_exception = serialize_exception
+http.JsonRPCDispatcher._response = _response
