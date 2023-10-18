3c68ccd887023f85801fb81192a6ab83c2183e21
yibudak <yibudak@gmail.com>
2023-10-18 17:58:32 +0300

    [IMP] Vadeli calisan cari computation

--- altinkaya_invoice/models/res_partner.py
+++ altinkaya_invoice/models/res_partner.py
@@ -2,31 +2,69 @@ from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    z_muhasebe_kodu = fields.Char('Zirve Muhasebe kodu', size=64, required=False, translate=False)
-    z_receivable_export = fields.Char('Receivable Export', size=64, required=False)
-    z_payable_export = fields.Char('Payable Export', size=64, required=False)
-    purchase_default_account_id = fields.Many2one('account.account', string='Purchase Default Account', required=False,
-                                                  help=u"Satın alma işlemlerinde varsayılan muhasebe hesabı.")
-    accounting_contact = fields.Many2one('res.partner', string='Accounting Contact', required=False)
-    devir_yapildi = fields.Boolean('Devir Yapıldı', default=False)
+    def _search_due_working(self, operator, value):
+        operand = ">" if operator == "=" else "<="
+        partners = self.search(
+            [
+                ("property_payment_term_id.line_ids.days", operand, 0),
+            ],
+        )
+        return [("id", "in", partners.ids)]
+
+    z_muhasebe_kodu = fields.Char(
+        "Zirve Muhasebe kodu", size=64, required=False, translate=False
+    )
+    z_receivable_export = fields.Char("Receivable Export", size=64, required=False)
+    z_payable_export = fields.Char("Payable Export", size=64, required=False)
+    purchase_default_account_id = fields.Many2one(
+        "account.account",
+        string="Purchase Default Account",
+        required=False,
+        help="Satın alma işlemlerinde varsayılan muhasebe hesabı.",
+    )
+    accounting_contact = fields.Many2one(
+        "res.partner", string="Accounting Contact", required=False
+    )
+    devir_yapildi = fields.Boolean("Devir Yapıldı", default=False)
+    vadeli_calisiyor = fields.Boolean(
+        "Vadeli Çalışıyor",
+        compute="_compute_due_working",
+        store=False,
+        default=False,
+        search="_search_due_working",
+    )
+
+    @api.model
+    def _compute_due_working(self):
+        for record in self:
+            record.vadeli_calisiyor = (
+                0 not in record.property_payment_term_id.line_ids.mapped("days")
+            )
 
     @api.model
     def create(self, vals):
-        if not vals.get('ref') and self._needsRef(vals=vals):
-            vals['ref'] = self._get_next_ref(vals=vals)
-            if vals.get('ref') and vals.get('country_id') and vals.get('customer') or vals.get('supplier'):
-                country_id = self.env['res.country'].browse(vals['country_id'])
-                if country_id and country_id.code != 'TR':
-                    z_receivable_export = '120.Y%s' % (vals['ref'].strip() or '')
-                    z_payable_export = '320.Y%s' % (vals['ref'].strip() or '')
+        if not vals.get("ref") and self._needsRef(vals=vals):
+            vals["ref"] = self._get_next_ref(vals=vals)
+            if (
+                vals.get("ref")
+                and vals.get("country_id")
+                and vals.get("customer")
+                or vals.get("supplier")
+            ):
+                country_id = self.env["res.country"].browse(vals["country_id"])
+                if country_id and country_id.code != "TR":
+                    z_receivable_export = "120.Y%s" % (vals["ref"].strip() or "")
+                    z_payable_export = "320.Y%s" % (vals["ref"].strip() or "")
                 else:
-                    z_receivable_export = '120.%s' % (vals['ref'].strip() or '')
-                    z_payable_export = '320.%s' % (vals['ref'].strip() or '')
-                vals.update({
-                    'ref': vals['ref'],
-                    'z_receivable_export': z_receivable_export,
-                    'z_payable_export': z_payable_export
-                })
+                    z_receivable_export = "120.%s" % (vals["ref"].strip() or "")
+                    z_payable_export = "320.%s" % (vals["ref"].strip() or "")
+                vals.update(
+                    {
+                        "ref": vals["ref"],
+                        "z_receivable_export": z_receivable_export,
+                        "z_payable_export": z_payable_export,
+                    }
+                )
         return super(ResPartner, self).create(vals)
