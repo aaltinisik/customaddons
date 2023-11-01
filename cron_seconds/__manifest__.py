14232be80acbd9fcc1c930cfaf0d5dac93c8f7e1
yibudak <yibudak@gmail.com>
2023-11-01 20:28:55 +0300

    [ADD] cron_seconds

---
+++ cron_seconds/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Yiğit Budak (https://github.com/yibudak)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+{
+    "name": "Cron in Seconds",
+    "summary": "Run cron jobs in seconds",
+    "description": """
+    This module adds the ability to run cron jobs in seconds.
+    """,
+    "version": "16.0.1.0.0",
+    "author": "Yiğit Budak",
+    "website": "https://github.com/yibudak",
+    "license": "LGPL-3",
+    "category": "Tools",
+    "depends": [
+        "base",
+    ],
+    "data": [],
+    "installable": True,
+}
