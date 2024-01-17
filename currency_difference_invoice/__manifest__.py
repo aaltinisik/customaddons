9b2a82a359495381e396826803ee84415a953dae
yibudak <yibudak@gmail.com>
2024-01-17 16:38:56 +0300

    [IMP] Currency valuation compute

--- currency_difference_invoice/__manifest__.py
+++ currency_difference_invoice/__manifest__.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "currency_difference_invoice",
-
-    'summary': """
+    "name": "Currency Difference Invoice",
+    "summary": """
         This module is for creating invoice with difference currency amount""",
-
-    'description': """
+    "description": """
         * Calculate currency difference amount
         * Create an invoice with calculated amount
-        
+        * Calculate currency valuation amount for foreign currency partners
     """,
-
-    'author': "yibudak",
-    'website': "https://github.com/yibudak",
-    'category': 'Accounting',
-    'version': '0.1',
-
-    'depends': ['base', 'account'],
-
-    'data': [
-        'views/res_partner_view.xml',
-        'views/res_company_view.xml',
-        'views/account_invoice_view.xml',
-        'wizard/create_currency_difference_invoices.xml',
-        'wizard/account_invoice_switch_incomings.xml',
+    "author": "Yiğit Budak",
+    "website": "https://github.com/yibudak",
+    "category": "Accounting",
+    "version": "0.1",
+    "depends": ["base", "account"],
+    "data": [
+        "views/res_partner_view.xml",
+        "views/res_company_view.xml",
+        "views/account_invoice_view.xml",
+        "wizard/create_currency_difference_invoices.xml",
+        "wizard/account_invoice_switch_incomings.xml",
+        "wizard/create_currency_valuation_move.xml",
     ],
 }
