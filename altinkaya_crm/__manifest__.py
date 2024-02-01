0947d33a44469297a96ff8057beff25a5bfce5d5
Yusuf <yoyosheta@gmail.com>
2024-02-01 13:11:58 +0300

    [MIG] altinkaya_crm

---
+++ altinkaya_crm/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 Yousef Sheta (https://github.com/TrueYouface)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Altinkaya CRM Extension",
+    "summary": "Adds tracking and conversion rates to orders.",
+    "description": "This is a CRM module made for Altinkaya.",
+    "version": "12.0.1.0.0",
+    "category": "General",
+    "website": "https://github.com/TrueYouface",
+    "author": "Yousef Sheta",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["crm", "sale"],
+    "data": [
+        "views/crm_lead_view.xml",
+        "views/sale_order_view.xml",
+        "views/res_partner_view.xml",
+    ],
+}
---
+++ altinkaya_crm/models/crm_lead.py
@@ -0,0 +1,37 @@
+from odoo import models, fields, api
+
+
+class CRMLead(models.Model):
+    _inherit = "crm.lead"
+
+    linkedin = fields.Char(string="LinkedIn")
+
+    @api.model
+    def _search_my_team_activity(self, operator, operand):
+        # if operator == "=":
+        #     new_operator = "in"
+        # else:
+        #     new_operator = "not in"
+        res = self.search(
+            [
+                ("team_id.member_ids", operator, self.env.user.id),
+            ],
+        )
+        return [("id", "in", res.ids)]
+
+    my_team_activity = fields.Boolean(
+        "My Team Activity",
+        compute="_compute_my_team_activity",
+        search="_search_my_team_activity",
+        store=False,
+    )
+
+    def _compute_my_team_activity(self):
+        for lead in self:
+            if self.env.user in lead.team_id.member_ids:
+                lead.my_team_activity = True
+            else:
+                lead.my_team_activity = False
+
+
+
