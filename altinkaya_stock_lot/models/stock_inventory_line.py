1e1716fde6355c8df686368a43b09a0d8acec201
yibudak <yibudak@gmail.com>
2024-03-13 11:49:21 +0300

    [FIX] Stock inventory line negative quantity

--- altinkaya_stock_lot/models/stock_inventory_line.py
+++ altinkaya_stock_lot/models/stock_inventory_line.py
@@ -1,5 +1,6 @@
 from odoo import api, models
 
+
 class InventoryLine(models.Model):
     _inherit = "stock.inventory.line"
 
@@ -8,10 +9,23 @@ class InventoryLine(models.Model):
         """EXPERIMENTAL: Create a lot for the move line if it is missing."""
         for rec in self:
             if rec.product_id.tracking != "none" and not rec.prod_lot_id:
+                # yigit: When working with negative quantities, any lot without quant
+                # is causing issues, try to search quant and link it to lot
+                related_quant = rec.env["stock.quant"].search(
+                    [
+                        ("product_id", "=", rec.product_id.id),
+                        ("location_id", "=", rec.inventory_location_id.id),
+                        ("quantity", "=", rec.product_qty),
+                        ("lot_id", "=", False),
+                    ],
+                    limit=1,
+                )
                 prod_lot_id = self.env["stock.production.lot"].create(
                     {
                         "product_id": rec.product_id.id,
                         "ref": rec.inventory_id.name or "",
+                        "product_qty": rec.product_qty,
+                        "quant_ids": [(6, 0, related_quant.ids)],
                     }
                 )
                 rec.prod_lot_id = prod_lot_id.id
